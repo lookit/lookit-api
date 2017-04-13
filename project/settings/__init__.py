6f4de139bed4115955a6c5753c81c9fce6407501
Chris Wisecarver <cwisecarver@cos.io>
2017-04-12 20:20:00 -0400

    Initial commit

---
+++ project/settings/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+'''Consolidates settings from defaults.py and local.py.
+'''
+import os
+import warnings
+import itertools
+
+from .defaults import *  # noqa
+
+try:
+    from .local import *  # noqa
+except ImportError as error:
+    warnings.warn('No project/settings/local.py settings file found. Did you remember to '
+                  'copy local-dist.py to local.py?', ImportWarning)
