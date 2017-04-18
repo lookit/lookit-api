e9ebdf1efe7438bf2bb57c5bff2e1bf338bfac5d
Chris Wisecarver <cwisecarver@cos.io>
2017-04-18 11:42:21 -0400

    Add some apps and delete things that are deprecated.

--- studies/models.py
+++ studies/models.py
@@ -3,14 +3,5 @@ from django.db import models
 from project.fields.datetime_aware_jsonfield import DateTimeAwareJSONField
 
 
-class FrameType(models.Model):
-    pass
-
-
-class Frame(models.Model):
-    kind = models.ForeignKey(FrameType, on_delete=models.CASCADE, related_name='frame')
-    blocks = DateTimeAwareJSONField(default=list)
-
-
 class Study(models.Model):
-    frames = models.ManyToManyField(Frame, related_name='study', limit_choices_to=None, through=None)
+    blocks = DateTimeAwareJSONField(default=list)
