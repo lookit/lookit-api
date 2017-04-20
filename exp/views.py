85a3354ac6d605ae70017ec3cd84b62cf4d589fa
Chris Wisecarver <cwisecarver@cos.io>
2017-04-20 15:22:19 -0400

    Correctly set initial values for users

--- exp/views.py
+++ exp/views.py
@@ -33,8 +33,6 @@ class UserDetailView(generic.UpdateView):
         return retval
 
 
-
-
 class AssignUserStudies(generic.UpdateView):
     template_name = 'accounts/assign_studies_form.html'
     queryset = User.objects.filter(participant__isnull=True)
@@ -61,8 +59,24 @@ class UserCreateView(generic.CreateView):
         'username',
         'given_name',
         'middle_name',
-        'family_name'
+        'family_name',
+        'is_active',
+        'is_staff',
+        'is_superuser',
+        'password'
     )
+
+    def post(self, request, *args, **kwargs):
+        # TODO put this on the view so that we can send the user an email once their user is saved
+        self.user_password = User.objects.make_random_password(length=12)
+        form = self.get_form()
+        query_dict = form.data.copy()
+        query_dict.update(is_active=True, is_superuser=False, is_staff=False, password=self.user_password)
+        form.data = query_dict
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
 
     def get_success_url(self):
         return reverse('assign-studies', kwargs={'pk': self.object.id})
