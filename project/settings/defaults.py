6fe6768aee2923ba4ec6585d80122af2c667a12f
Chris Wisecarver <cwisecarver@cos.io>
2017-06-15 16:04:56 -0400

    Revert to basic DRF JSON API

--- project/settings/defaults.py
+++ project/settings/defaults.py
@@ -152,7 +152,7 @@ REST_FRAMEWORK = {
         'rest_framework.parsers.MultiPartParser'
     ),
     'DEFAULT_RENDERER_CLASSES': (
-        'api.renderers.JSONRenderer',
+        'rest_framework_json_api.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
     ),
     'DEFAULT_METADATA_CLASS': 'rest_framework_json_api.metadata.JSONAPIMetadata',
