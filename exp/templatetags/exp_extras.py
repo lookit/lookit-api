4ae460e11b0d1a08a07350c66db3d65a449ac758
Dawn Pattison <pattison.dawn.r@gmail.com>
2017-06-15 17:02:21 -0400

    Update query params for sort and match.

--- exp/templatetags/exp_extras.py
+++ exp/templatetags/exp_extras.py
@@ -7,4 +7,8 @@ def query_transform(request, **kwargs):
     updated = request.GET.copy()
     if kwargs.get('state'):
         updated['state'] = kwargs.get('state')
+    if kwargs.get('match'):
+        updated['match'] = kwargs.get('match')
+    if kwargs.get('sort'):
+        updated['sort'] = kwargs.get('sort')
     return updated.urlencode()
