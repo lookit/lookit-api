e4a9719b105be6089bfec9212f54759a80e0c467
Dawn Pattison <pattison.dawn.r@gmail.com>
2017-06-27 14:42:35 -0400

    Add build_study_group_name method.

--- accounts/utils.py
+++ accounts/utils.py
@@ -3,3 +3,7 @@ from django.utils.text import slugify
 def build_org_group_name(org_name, group):
     """Returns org group name in the form of <ORGNAME>_ORG_<GROUP>"""
     return f'{slugify({org_name})}_ORG_{group}'.upper()
+
+def build_study_group_name(org_name, study_name, group):
+    """Returns org group name in the form of <ORGNAME>_ORG_<GROUP>"""
+    return f'{slugify({org_name})}_{slugify({study_name})}_STUDY_{group}'.upper()
