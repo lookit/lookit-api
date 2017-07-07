a196e0f2e6892f154260da590ef47a4862c5ecab
Henrique Harman <henriqueharman@gmail.com>
2017-07-07 16:47:14 -0400

    Add logic for showing partcipation or requirement buttons

--- web/views.py
+++ web/views.py
@@ -55,6 +55,15 @@ class StudyDetailView(generic.DetailView):
                           {'verbose_name': queryset.model._meta.verbose_name})
         return obj
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user'] = self.request.user
+        if self.request.user.is_authenticated:
+            context['has_demographic'] = self.request.user.latest_demographics
+            context['children'] = self.request.user.children.all()
+
+        return context
+
 
 class ParticipantSignupView(generic.CreateView):
     '''
