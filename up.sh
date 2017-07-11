e4874825cef225a98a2c2d01e754dbdd2c81d819
Dawn Pattison <pattison.dawn.r@gmail.com>
2017-07-11 07:40:41 -0500

    Remove up.sh modification.

--- up.sh
+++ up.sh
@@ -5,4 +5,4 @@ pip install -r requirements/dev.txt
 
 psql -U postgres -tc "SELECT 1 FROM pg_database WHERE datname = 'lookit'" | grep -q 1 || psql -U postgres -c "CREATE DATABASE lookit"
 
-/Users/henriqueharman/.virtualenvs/lookit/bin/python3.6 manage.py migrate
+python manage.py migrate
