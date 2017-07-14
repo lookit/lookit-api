9bb1b56402d3c1cfcce2c651fc603bd9cf4d3655
Barrett K. Harber <barrett.harber@gmail.com>
2017-07-14 16:56:38 -0400

    Add docker, update wsgi & requirements to match

--- project/wsgi.py
+++ project/wsgi.py
@@ -7,6 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
+from gevent import monkey
+monkey.patch_all()
+
+from psycogreen.gevent import patch_psycopg  # noqa
+patch_psycopg()
+
 import os
 
 from django.core.wsgi import get_wsgi_application
