35d2e92c2586774eb966d2a1ec8223b9c51f61a8
Chris Wisecarver <cwisecarver@cos.io>
2017-07-17 12:26:28 -0400

    Remove LOOKIT_ from envvars

--- project/settings/defaults.py
+++ project/settings/defaults.py
@@ -114,11 +114,11 @@ DATABASES = {
     'default': {
         'CONN_MAX_AGE': 0,
         'ENGINE': 'django.db.backends.postgresql',  # django.db.backends.postgresql
-        'NAME': os.environ.get('LOOKIT_DB_NAME', 'lookit'),
-        'USER': os.environ.get('LOOKIT_DB_USER', 'postgres'),
-        'PASSWORD': os.environ.get('LOOKIT_DB_PASSWORD', ''),
-        'HOST': os.environ.get('LOOKIT_DB_HOST', '127.0.0.1'),
-        'PORT': os.environ.get('LOOKIT_DB_PORT', '5432'),
+        'NAME': os.environ.get('DB_NAME', 'lookit'),
+        'USER': os.environ.get('DB_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+        'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
         'ATOMIC_REQUESTS': True,
     }
 }
