84c691cbcfba316c82f2c52d77e72f3a6f6d5621
Chris Wisecarver <cwisecarver@cos.io>
2017-07-19 14:53:23 -0400

    Add video recorder proxy

--- web/urls.py
+++ web/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     url(r'^contact_us/?$', flatpages_views.flatpage, dict(url='/contact_us/'), name='contact_us'),
     url(r'^(?P<path>assets/.*)$', views.ExperimentAssetsProxyView.as_view(), name='experiment-assets-proxy'),
     url(r'^(?P<path>fonts/.*)$', views.ExperimentAssetsProxyView.as_view(), name='experiment-fonts-proxy'),
-    url(r'^(?P<path>ember-cli-live-reload.js)$', views.ExperimentAssetsProxyView.as_view(), name='ember-cli-live-reload-proxy'),  # we shouldn't need this in prod
+    url(r'^(?P<path>ember-cli-live-reload\.js)$', views.ExperimentAssetsProxyView.as_view(), name='ember-cli-live-reload-proxy'),  # we shouldn't need this in prod
+    url(r'^(?P<path>VideoRecorder\.swf)$', views.ExperimentAssetsProxyView.as_view(), name='videorecorder-proxy'),
     url(r'^$', flatpages_views.flatpage, dict(url='/'), name='home'),
 ]
