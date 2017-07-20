3c16ef93380757175769793afbe9e3b165b615e2
Dawn Pattison <pattison.dawn.r@gmail.com>
2017-07-20 18:27:28 -0500

    Correct preview proxy url.

--- exp/urls.py
+++ exp/urls.py
@@ -43,6 +43,6 @@ urlpatterns = [
     url(r'studies/(?P<pk>\d+)/$', StudyDetailView.as_view(), name='study-detail'),
     url(r'studies/(?P<pk>\d+)/edit/$', StudyUpdateView.as_view(), name='study-edit'),
     url(r'studies/(?P<pk>\d+)/edit/build/$', StudyBuildView.as_view(), name='study-build'),
-    url(r'^studies/(?P<study_uuid>[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89ab][0-9a-fA-F]{3}-[0-9a-fA-F]{12})/preview/?$', PreviewProxyView.as_view(), name='preview-proxy'),
+    url(r'^studies/(?P<path>(?P<uuid>[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89ab][0-9a-fA-F]{3}-[0-9a-fA-F]{12}))/preview/?$', PreviewProxyView.as_view(), name='preview-proxy'),
     url(r'', RedirectView.as_view(url=reverse_lazy('exp:study-list')), name='homepage')
 ]
