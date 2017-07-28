78928d3b709afd6ce5ac42d77ffb40e1d00b713b
Dawn Pattison <pattison.dawn.r@gmail.com>
2017-07-28 14:11:45 -0500

    Add comments to DemographicDataUpdateView and ParticipantUpdateView.

--- web/views.py
+++ web/views.py
@@ -16,6 +16,145 @@ from project import settings
 from studies.models import Study
 
 
+class ParticipantSignupView(generic.CreateView):
+    '''
+    Allows a participant to sign up. Redirects them to a page to add their demographic data.
+    '''
+    template_name = 'web/participant-signup.html'
+    model = User
+    form_class = forms.ParticipantSignupForm
+
+    def form_valid(self, form):
+        resp = super().form_valid(form)
+        new_user = authenticate(
+            self.request,
+            username=form.cleaned_data['username'],
+            password=form.cleaned_data['password1']
+        )
+        login(self.request, new_user, backend='django.contrib.auth.backends.ModelBackend')
+        return resp
+
+    def get_success_url(self):
+        return reverse('web:demographic-data-update')
+
+
+class DemographicDataUpdateView(LoginRequiredMixin, generic.CreateView):
+    """
+    Allows user to update demographic data - but actually creates new version instead of updating old one.
+    """
+    template_name = 'web/demographic-data-update.html'
+    model = DemographicData
+    form_class = forms.DemographicDataForm
+
+    def get_success_url(self):
+        return reverse('web:demographic-data-update')
+
+    def form_valid(self, form):
+        """
+        Before saving form, adds user relationship to demographic data, and sets "previous"
+         as the last saved demographic data.
+        """
+        resp = super().form_valid(form)
+        self.object.user = self.request.user
+        self.object.previous = self.request.user.latest_demographics or None
+        self.object.save()
+        return resp
+
+    def get_initial(self):
+        """
+        Returns the initial data to use for forms on this view. Prepopulates
+        demographic data form with the latest demographic data.
+        """
+        demographic_data = self.request.user.latest_demographics or None
+        if demographic_data:
+            demographic_data_dict = demographic_data.__dict__
+            demographic_data_dict.pop('id')
+            demographic_data_dict.pop('uuid')
+            return demographic_data_dict
+        return demographic_data
+
+    def get_success_url(self):
+        return reverse('web:studies-list')
+
+
+class ParticipantUpdateView(LoginRequiredMixin, generic.UpdateView):
+    """
+    Allows a participant to update their name and password -
+    extra code in this view because there are multiple forms on this page.
+    """
+    template_name = 'web/participant-update.html'
+    model = User
+    form_class = forms.ParticipantUpdateForm
+    second_form_class = forms.ParticipantPasswordForm
+
+    def get_context_data(self, **kwargs):
+        """
+        Adds the context for form1 and form2 on the page - a little extra code due to the
+        two forms on the page.  The form that was not edited is unbound so data
+        is not validated.
+        """
+        context = super().get_context_data(**kwargs)
+        user = self.request.user
+        if 'participant_update' in self.request.POST:
+            context['form2'].is_bound = False
+        if 'password_update' in self.request.POST:
+            context['form'].is_bound = False
+            context['form'].initial = {'username': user.username, 'given_name': user.given_name, 'middle_name': user.middle_name, 'family_name': user.family_name}
+        if 'form' not in context:
+            context['form'] = self.form_class(self.request.GET)
+        if 'form2' not in context:
+            context['form2'] = self.second_form_class(self.request.POST)
+        return context
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+    def get_success_url(self):
+        return reverse('web:participant-update')
+
+    def get_form_kwargs(self):
+        """
+        Returns the keyword arguments for instantiating the form. -
+        if updating password section, need the 'user' kwarg
+        """
+        kwargs = super().get_form_kwargs()
+        if 'password_update' in self.request.POST:
+            kwargs['user'] =  kwargs.pop('instance')
+        else:
+            if 'user' in kwargs:
+                kwargs.pop('user')
+        return kwargs
+
+    def form_invalid(self, **kwargs):
+        return self.render_to_response(self.get_context_data(**kwargs))
+
+    def post(self, request, *args, **kwargs):
+        """
+        Returns the keyword arguments for instantiating the form. -
+        if updating password section, need the 'user' kwarg
+        """
+        self.object = self.get_object()
+        # Depending on the keywords in the POST, choose the participant update
+        # or the password update form.
+        if 'participant_update' in request.POST:
+            form_class = self.get_form_class()
+            form_name = 'form'
+        if 'password_update' in request.POST:
+            form_class = self.second_form_class
+            form_name = 'form2'
+
+        form = self.get_form(form_class)
+        if form.is_valid():
+            form.save()
+            if form_name == 'form2':
+                # If updating password, need to reauthenticate
+                update_session_auth_hash(self.request, form.user)
+                return HttpResponseRedirect(self.get_success_url())
+            return super().form_valid(form)
+        else:
+            return self.form_invalid(**{form_name: form})
+
+
 class StudiesListView(generic.ListView):
     '''
     List all active studies.
@@ -68,28 +207,6 @@ class StudyDetailView(generic.DetailView):
             context['children'] = self.request.user.children.all()
 
         return context
-
-
-class ParticipantSignupView(generic.CreateView):
-    '''
-    Allows a participant to sign up
-    '''
-    template_name = 'web/participant-signup.html'
-    model = User
-    form_class = forms.ParticipantSignupForm
-
-    def form_valid(self, form):
-        resp = super().form_valid(form)
-        new_user = authenticate(
-            self.request,
-            username=form.cleaned_data['username'],
-            password=form.cleaned_data['password1']
-        )
-        login(self.request, new_user, backend='django.contrib.auth.backends.ModelBackend')
-        return resp
-
-    def get_success_url(self):
-        return reverse('web:demographic-data-update')
 
 
 class ChildrenListView(LoginRequiredMixin, generic.CreateView):
@@ -160,40 +277,6 @@ class ChildUpdateView(LoginRequiredMixin, generic.UpdateView):
         return super().post(request, *args, **kwargs)
 
 
-class DemographicDataUpdateView(LoginRequiredMixin, generic.CreateView):
-    """
-    Allows user to update demographic data - but actually creates new version instead of updating old one.
-    """
-    template_name = 'web/demographic-data-update.html'
-    model = DemographicData
-    form_class = forms.DemographicDataForm
-
-    def get_success_url(self):
-        return reverse('web:demographic-data-update')
-
-    def form_valid(self, form):
-        resp = super().form_valid(form)
-        self.object.user = self.request.user
-        self.object.previous = self.request.user.latest_demographics or None
-        self.object.save()
-        return resp
-
-    def get_initial(self):
-        """
-        Returns the initial data to use for forms on this view.
-        """
-        demographic_data = self.request.user.latest_demographics or None
-        if demographic_data:
-            demographic_data_dict = demographic_data.__dict__
-            demographic_data_dict.pop('id')
-            demographic_data_dict.pop('uuid')
-            return demographic_data_dict
-        return demographic_data
-
-    def get_success_url(self):
-        return reverse('web:studies-list')
-
-
 class ParticipantEmailPreferencesView(LoginRequiredMixin, generic.UpdateView):
     """
     Allows a participant to update their email preferences - when they can be contacted.
@@ -207,67 +290,6 @@ class ParticipantEmailPreferencesView(LoginRequiredMixin, generic.UpdateView):
 
     def get_success_url(self):
         return reverse('web:email-preferences')
-
-
-class ParticipantUpdateView(LoginRequiredMixin, generic.UpdateView):
-    """
-    Allows a participant to update their names and password
-    """
-    template_name = 'web/participant-update.html'
-    model = User
-    form_class = forms.ParticipantUpdateForm
-    second_form_class = forms.ParticipantPasswordForm
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        user = self.request.user
-        if 'participant_update' in self.request.POST:
-            context['form2'].is_bound = False
-        if 'password_update' in self.request.POST:
-            context['form'].is_bound = False
-            context['form'].initial = {'username': user.username, 'given_name': user.given_name, 'middle_name': user.middle_name, 'family_name': user.family_name}
-        if 'form' not in context:
-            context['form'] = self.form_class(self.request.GET)
-        if 'form2' not in context:
-            context['form2'] = self.second_form_class(self.request.POST)
-        return context
-
-    def get_object(self, queryset=None):
-        return self.request.user
-
-    def get_success_url(self):
-        return reverse('web:participant-update')
-
-    def get_form_kwargs(self):
-        kwargs = super().get_form_kwargs()
-        if 'password_update' in self.request.POST:
-            kwargs['user'] =  kwargs.pop('instance')
-        else:
-            if 'user' in kwargs:
-                kwargs.pop('user')
-        return kwargs
-
-    def form_invalid(self, **kwargs):
-        return self.render_to_response(self.get_context_data(**kwargs))
-
-    def post(self, request, *args, **kwargs):
-        self.object = self.get_object()
-        if 'participant_update' in request.POST:
-            form_class = self.get_form_class()
-            form_name = 'form'
-        if 'password_update' in request.POST:
-            form_class = self.second_form_class
-            form_name = 'form2'
-
-        form = self.get_form(form_class)
-        if form.is_valid():
-            form.save()
-            if form_name == 'form2':
-                update_session_auth_hash(self.request, form.user)
-                return HttpResponseRedirect(self.get_success_url())
-            return super().form_valid(form)
-        else:
-            return self.form_invalid(**{form_name: form})
 
 
 class ExperimentAssetsProxyView(ProxyView, LoginRequiredMixin):
