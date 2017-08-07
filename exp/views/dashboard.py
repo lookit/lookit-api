9eded44a139d5affaf3b14dc2f3d77a9b83b30cc
Dawn Pattison <pattison.dawn.r@gmail.com>
2017-08-07 18:56:50 -0500

    Have dashboard view redirect if no trailing slash [WIP].

--- exp/views/dashboard.py
+++ exp/views/dashboard.py
@@ -14,6 +14,8 @@ class ExperimenterDashboardView(ExperimenterLoginRequiredMixin, generic.Template
     template_name = 'exp/dashboard.html'
 
     def dispatch(self, request, *args, **kwargs):
-        if self.request.user.groups.exists():
-            return redirect(reverse_lazy('exp:study-list'))
-        return super().dispatch(request, *args, **kwargs)
+        if self.request.path.endswith('/'):
+            if self.request.user.groups.exists():
+                return redirect(reverse_lazy('exp:study-list'))
+            return super().dispatch(request, *args, **kwargs)
+        return redirect(self.request.path + '/')
