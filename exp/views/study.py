8b587d8fd263b23f5cf723a6b4245a7d5e7c8f89
Dawn Pattison <pattison.dawn.r@gmail.com>
2017-08-10 16:32:30 -0500

    Don't show preview data.

--- exp/views/study.py
+++ exp/views/study.py
@@ -446,7 +446,7 @@ class StudyResponsesList(ExperimenterLoginRequiredMixin, PermissionRequiredMixin
 
     def get_study_attachments(self, study):
         attachments = get_study_attachments.get_all_study_attachments(str(study.uuid))
-        return attachments
+        return [ attachment for attachment in attachments if "PREVIEW_DATA_DISREGARD" not in attachment ]
 
 
 class PreviewProxyView(ProxyView, ExperimenterLoginRequiredMixin):
