60ef934e3bef7c00fc2d1823901babb665a4888f
Dawn Pattison <pattison.dawn.r@gmail.com>
2017-08-10 16:43:23 -0500

    Remove looping through items and appending them to list.

--- get_study_attachments.py
+++ get_study_attachments.py
@@ -5,12 +5,8 @@ BUCKET_NAME = 'mitLookit'
 
 def get_all_study_attachments(study_uuid):
     s3 = boto3.resource('s3')
-
     bucket = s3.Bucket(BUCKET_NAME)
-    study_files = []
-    for key in bucket.objects.filter(Prefix=f'videoStream_{study_uuid}'):
-        study_files.append(key)
-    return study_files
+    return bucket.objects.filter(Prefix=f'videoStream_{study_uuid}')
 
 if __name__ == '__main__':
     study_uuid = sys.argv[1]
