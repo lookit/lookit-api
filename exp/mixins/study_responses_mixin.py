b3963a055121e07d08555c76d0fb429f1b82e597
Dawn Pattison <pattison.dawn.r@gmail.com>
2017-08-16 12:16:43 -0500

    Pull demographic data out of study responses and add children details instead.

--- exp/mixins/study_responses_mixin.py
+++ exp/mixins/study_responses_mixin.py
@@ -32,40 +32,67 @@ class StudyResponsesMixin(ExperimenterLoginRequiredMixin, PermissionRequiredMixi
         for resp in responses:
             latest_dem = resp.demographic_snapshot
             json_responses.append(json.dumps({
-                'sequence': resp.sequence,
-                'conditions': resp.conditions,
-                'exp_data': resp.exp_data,
-                'participant_id': resp.child.user.id,
-                'global_event_timings': resp.global_event_timings,
-                'child_id': resp.child.id,
-                'completed': resp.completed,
-                'study_id': resp.study.id,
-                'response_id': resp.id,
+                'response': {
+                    'id': resp.id,
+                    'sequence': resp.sequence,
+                    'conditions': resp.conditions,
+                    'exp_data': resp.exp_data,
+                    'global_event_timings': resp.global_event_timings,
+                    'completed': resp.completed,
+                },
+                'study': {
+                    'id': resp.study.id
+                },
                 'participant': {
-                    "participant_id": resp.child.user.id,
-                    "demographics_info": {
-                        "demographic_id": latest_dem.id,
-                        "number_of_children": latest_dem.number_of_children,
-                        "child_birthdays": latest_dem.child_birthdays,
-                        "languages_spoken_at_home": latest_dem.languages_spoken_at_home,
-                        "number_of_guardians": latest_dem.number_of_guardians,
-                        "number_of_guardians_explanation": latest_dem.number_of_guardians_explanation,
-                        "race_identification": latest_dem.race_identification,
-                        "age": latest_dem.age,
-                        "gender": latest_dem.gender,
-                        "education_level": latest_dem.gender,
-                        "spouse_education_level": latest_dem.spouse_education_level,
-                        "annual_income": latest_dem.annual_income,
-                        "number_of_books": latest_dem.number_of_books,
-                        "additional_comments": latest_dem.additional_comments,
-                        "country": latest_dem.country.name,
-                        "state": latest_dem.state,
-                        "density": latest_dem.density,
-                        "extra": latest_dem.extra
-                    }
+                    'id': resp.child.user_id,
+                    'username': resp.child.user.given_name
+                },
+                'child': {
+                    'id': resp.child.id,
+                    'name': resp.child.given_name,
+                    'birthday': resp.child.birthday,
+                    'gender': resp.child.gender,
+                    'age_at_birth': resp.child.age_at_birth,
+                    'additional_information': resp.child.additional_information
                 }}, indent=4, default = self.convert_to_string))
         return json_responses
-
+# 'participant': {
+#     "participant_id": resp.child.user.id,
+#     "demographics_info": {
+#         "demographic_id": latest_dem.id,
+#         "number_of_children": latest_dem.number_of_children,
+#         "child_birthdays": latest_dem.child_birthdays,
+#         "languages_spoken_at_home": latest_dem.languages_spoken_at_home,
+#         "number_of_guardians": latest_dem.number_of_guardians,
+#         "number_of_guardians_explanation": latest_dem.number_of_guardians_explanation,
+#         "race_identification": latest_dem.race_identification,
+#         "age": latest_dem.age,
+#         "gender": latest_dem.gender,
+#         "education_level": latest_dem.gender,
+#         "spouse_education_level": latest_dem.spouse_education_level,
+#         "annual_income": latest_dem.annual_income,
+#         "number_of_books": latest_dem.number_of_books,
+#         "additional_comments": latest_dem.additional_comments,
+#         "country": latest_dem.country.name,
+#         "state": latest_dem.state,
+#         "density": latest_dem.density,
+#         "extra": latest_dem.extra
+#     }
+    # return [resp.sequence, resp.conditions, resp.exp_data, resp.child.user.id,
+    #     resp.global_event_timings, resp.child.id, resp.completed, resp.study.id, resp.id,
+    #     resp.demographic_snapshot.id, latest_dem.number_of_children, [self.convert_to_string(birthday) for birthday in latest_dem.child_birthdays],
+    #     latest_dem.languages_spoken_at_home, latest_dem.number_of_guardians, latest_dem.number_of_guardians_explanation,
+    #     latest_dem.race_identification, latest_dem.age, latest_dem.gender, latest_dem.education_level, latest_dem.spouse_education_level,
+    #     latest_dem.annual_income, latest_dem.number_of_books, latest_dem.additional_comments, latest_dem.country.name,
+    #     latest_dem.state, latest_dem.density, latest_dem.extra
+    # ]
+    # return [resp.sequence, resp.conditions, resp.exp_data, resp.child.user.id,
+    #     resp.global_event_timings, resp.child.id, resp.completed, resp.study.id, resp.id,
+    #     resp.demographic_snapshot.id, latest_dem.number_of_children, [self.convert_to_string(birthday) for birthday in latest_dem.child_birthdays],
+    #     latest_dem.languages_spoken_at_home, latest_dem.number_of_guardians, latest_dem.number_of_guardians_explanation,
+    #     latest_dem.race_identification, latest_dem.age, latest_dem.gender, latest_dem.education_level, latest_dem.spouse_education_level,
+    #     latest_dem.annual_income, latest_dem.number_of_books, latest_dem.additional_comments, latest_dem.country.name,
+    #     latest_dem.state, latest_dem.density, latest_dem.extra
     def csv_output_and_writer(self):
         output = io.StringIO()
         return output, csv.writer(output, quoting=csv.QUOTE_NONNUMERIC)
@@ -74,25 +101,19 @@ class StudyResponsesMixin(ExperimenterLoginRequiredMixin, PermissionRequiredMixi
         """
         Builds individual row for csv responses
         """
-        latest_dem = resp.demographic_snapshot
-        return [resp.sequence, resp.conditions, resp.exp_data, resp.child.user.id,
-            resp.global_event_timings, resp.child.id, resp.completed, resp.study.id, resp.id,
-            resp.demographic_snapshot.id, latest_dem.number_of_children, [self.convert_to_string(birthday) for birthday in latest_dem.child_birthdays],
-            latest_dem.languages_spoken_at_home, latest_dem.number_of_guardians, latest_dem.number_of_guardians_explanation,
-            latest_dem.race_identification, latest_dem.age, latest_dem.gender, latest_dem.education_level, latest_dem.spouse_education_level,
-            latest_dem.annual_income, latest_dem.number_of_books, latest_dem.additional_comments, latest_dem.country.name,
-            latest_dem.state, latest_dem.density, latest_dem.extra
+        return [resp.id, resp.sequence, resp.conditions, resp.exp_data, resp.global_event_timings, resp.completed, resp.study.id,
+            resp.child.user.id, resp.child.user.given_name, resp.child.id, resp.child.given_name, resp.child.birthday, resp.child.gender,
+            resp.child.age_at_birth, resp.child.additional_information
         ]
+
 
     def get_csv_headers(self):
         """
         Returns header row for csv data
         """
-        return ['sequence', 'conditions', 'exp_data', 'participant_id', 'global_event_timings',
-            'child_id', 'completed', 'study_id', 'response_id', 'demographic_id', 'number_of_children',
-            'child_birthdays', 'languages_spoken_at_home', 'number_of_guardians', 'number_of_guardians_explanation',
-            'race_identification', 'age', 'gender', 'education_level', 'spouse_education_level', 'annual_income',
-            'number_of_books', 'additional_comments', 'country', 'state', 'density', 'extra']
+        return ['response_id', 'response_sequence', 'response_conditions', 'response_exp_data', 'response_global_event_timings',
+            'response_completed', 'study_id', 'participant_id', 'participant_username', 'child_id', 'child_name',
+            'child_birthday', 'child_gender', 'child_age_at_birth', 'child_additional_information']
 
     def post(self, request, *args, **kwargs):
         '''
