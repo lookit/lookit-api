721329dda87f4a98b4236928d8b7242981726deb
Dawn Pattison <pattison.dawn.r@gmail.com>
2017-08-28 09:29:36 -0500

    Add StudyType to admin app.

--- exp/admin.py
+++ exp/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from guardian.admin import GuardedModelAdmin
 
-from studies.models import Response, ResponseLog, Study, StudyLog, Feedback
+from studies.models import Response, ResponseLog, Study, StudyLog, Feedback, StudyType
 
 
 class StudyAdmin(GuardedModelAdmin):
@@ -20,8 +20,12 @@ class StudyLogAdmin(GuardedModelAdmin):
 class ResponseLogAdmin(GuardedModelAdmin):
     pass
 
+class StudyTypeAdmin(GuardedModelAdmin):
+    pass
+
 admin.site.register(Study, StudyAdmin)
 admin.site.register(Response, ResponseAdmin)
 admin.site.register(StudyLog, StudyLogAdmin)
 admin.site.register(ResponseLog, ResponseLogAdmin)
 admin.site.register(Feedback, FeedbackAdmin)
+admin.site.register(StudyType, StudyTypeAdmin)
