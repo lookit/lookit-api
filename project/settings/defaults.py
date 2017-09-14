b5a59416871232f36a781cd594ba8bb6f52f0efe
Michael Haselton <icereval@gmail.com>
2017-09-13 22:46:01 -0400

    Update defaults.py

--- project/settings/defaults.py
+++ project/settings/defaults.py
@@ -327,7 +327,7 @@ CELERY_TASK_ROUTES = {
     'studies.tasks.cleanup*': {'queue': 'cleanup'},
     'studies.helpers.send_mail': {'queue': 'email'}
 }
-CELERYBEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
+CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
 
 OSF_OAUTH_CLIENT_ID = os.environ.get('OSF_OAUTH_CLIENT_ID', '3518b74e12584abf9e48565ff6aee6f3')
 OSF_OAUTH_SECRET = os.environ.get('OSF_OAUTH_SECRET', 'vYlku3raTL5DnHZlkqCIaShmPVIl1nifsFJCNLxU')
