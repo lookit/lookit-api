160e66dfa21a0cfca850df8a86e442f91e152782
Kim Scott <kimscott@mit.edu>
2017-11-15 13:04:02 -0500

    have study creation form inherit from study edit form to more easily keep shared fields consistent *In review, please feel free to omit this commit if this is not good form!

--- studies/forms.py
+++ studies/forms.py
@@ -33,6 +33,7 @@ class BaseStudyForm(forms.ModelForm):
         return cleaned_data
 
 
+# Form for editing an existing study
 class StudyEditForm(BaseStudyForm):
 
     class Meta:
@@ -64,7 +65,9 @@ class StudyEditForm(BaseStudyForm):
             'criteria': 'Text shown to families - this is not used to actually verify eligibility.'
         }
 
-class StudyForm(BaseStudyForm):
+# Form for creating a new study. Set study_type and structure in same form
+# in this case, rather than separately in the build form.
+class StudyForm(StudyEditForm):
     structure = forms.CharField(label='Build Study - Add JSON', widget=AceOverlayWidget(mode='json', wordwrap=True, theme='textmate', width='100%', height='100%', showprintmargin=False), required=False, help_text='Add the frames of your study as well as the sequence of those frames.  This can be added later.')
 
     def clean_structure(self):
@@ -75,35 +78,14 @@ class StudyForm(BaseStudyForm):
              raise forms.ValidationError("Invalid JSON")
          return json_data
 
-    class Meta:
-        model = Study
-        fields = ['name', 'image', 'short_description', 'long_description', 'exit_url', 'criteria', 'min_age_years', 'min_age_months', 'min_age_days', 'max_age_years', 'max_age_months', 'max_age_days', 'duration', 'contact_info', 'public', 'structure', 'study_type']
-        labels = {
-            'short_description': 'Short Description',
-            'long_description': 'Purpose',
-            'exit_url': 'Exit URL',
-            'criteria': 'Participant Eligibility',
-            'contact_info': 'Researcher Contact Information',
-            'public': 'Discoverable - Do you want this study to be publicly discoverable on Lookit once activated?',
-            'study_type': 'Study Type'
-        }
-        widgets = {
-            'short_description': Textarea(attrs={'rows': 2}),
-            'long_description': Textarea(attrs={'rows': 2}),
-            'exit_url': Textarea(attrs={'rows': 1}),
-            'criteria': Textarea(attrs={'rows': 1}),
-            'duration': Textarea(attrs={'rows': 1}),
-            'contact_info': Textarea(attrs={'rows': 1}),
-        }
-        help_texts = {
-            'image': 'Please keep your file size less than 1 MB',
-            'exit_url': "Specify the page where you want to send your participants after they've completed the study.",
-            'short_description': 'Give your study a description here.',
-            'long_description': 'Explain the purpose of your study here.',
-            'study_type': "Specify the build process as well as the parameters needed by the experiment builder. If you don't know what this is, just select the default.",
-            'contact_info': 'This should give the name of the PI for your study, and an email address where the PI or study staff can be reached with questions. Format: PIs Name (contact: youremail@lab.edu)',
-        }
-
+    class Meta(StudyEditForm.Meta):
+        fields = StudyEditForm.Meta.fields + ['structure', 'study_type']
+        
+        labels = StudyEditForm.Meta.labels.copy()
+        labels['study_type'] = 'Study Type'
+        
+        help_texts = StudyEditForm.Meta.help_texts.copy()
+        help_texts['study_type'] = "Specify the build process as well as the parameters needed by the experiment builder. If you don't know what this is, just select the default."
 
 class StudyBuildForm(forms.ModelForm):
     structure = forms.CharField(label='Build Study - Add JSON', widget=AceOverlayWidget(mode='json', wordwrap=True, theme='textmate', width='100%', height='100%', showprintmargin=False), required=False, help_text='Add the frames of your study as well as the sequence of those frames.')
