af0d9f6429315746c04d9fd964155785ae59b4aa
Kim Scott <kimscott@mit.edu>
2017-12-22 17:15:37 -0500

    change rtd theme, add MIT to copyright going forward on this fork

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,14 +46,13 @@ source_parsers = {
     '.md': CommonMarkParser,
 }
 
-
 # The master toctree document.
 master_doc = 'index'
 
 # General information about the project.
 project = 'lookit-api'
-copyright = '2017, Center for Open Science'
-author = 'Center for Open Science'
+copyright = '2017, Center for Open Science; portions copyright MIT'
+author = 'Center for Open Science, MIT'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -88,7 +87,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
