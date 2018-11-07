3b67fa3899918fa30414a0d49da1ccdf0cf86fb3
Rico Rodriguez <ricorodriguez@dhcp-18-40-44-165.dyn.mit.edu>
2018-11-07 10:39:32 -0500

    More robust bootstrapping script

--- up.sh
+++ up.sh
@@ -1,5 +1,17 @@
 #! /bin/bash
 
+echo "Is your virtual environment up and running? [(Y|y)|(N/n)]"
+
+read answer
+
+if [[ "$answer" =~ ^(Y|y|YES|yes|Yes|)$ ]]; then
+  echo "Please activate your virtual environment so that pip installs dependencies in the correct location. (source {env name}/bin/activate, or activate.fish, .zsh, etc...)"
+  exit 1
+fi
+  
+# Ensure that GraphViz is installed correctly.
+sudo pip install pygraphviz --install-option="--include-path=/usr/include/graphviz" --install-option="--library-path=/usr/lib/graphviz/"
+
 pip install -r requirements/dev.txt
 
 psql -U postgres -tc "SELECT 1 FROM pg_database WHERE datname = 'lookit'" | grep -q 1 || psql -U postgres -c "CREATE DATABASE lookit"
