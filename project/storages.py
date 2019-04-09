b6b866060865732c261460eaa2f3f4b05ba1ea12
Rico Rodriguez <rico.l.rodriguez@gmail.com>
2019-04-08 21:33:10 -0400

    adding comments

--- project/storages.py
+++ project/storages.py
@@ -1,30 +1,5 @@
 from django.conf import settings
 from storages.backends.gcloud import GoogleCloudStorage
-from storages.utils import clean_name
-
-
-# class LocationPrefixedPublicGoogleCloudStorage(GoogleCloudStorage):
-#     location = None
-#
-#     def _normalize_name(self, name):
-#         if self.location:
-#             return super()._normalize_name(safe_join(self.location, name))
-#         return super()._normalize_name(name.lower())
-#
-#     def url(self, name):
-#         """
-#         The parent implementation calls GoogleCloudStorage on every request
-#         once for oauth and again to get the file. Since we're proxying requests
-#         through nginx's proxy_pass to GoogleCloudStorage we don't need their url
-#         nonsense or to use the actual domain or bucket name.
-#         """
-#         name = self._normalize_name(clean_name(name))
-#         return f"/{name.lstrip('/')}"
-#
-#
-# class LowercaseNameMixin(GoogleCloudStorage):
-#     def _normalize_name(self, name):
-#         return super()._normalize_name(name.lower())
 
 
 class LookitGoogleCloudStorage(GoogleCloudStorage):
@@ -34,11 +9,10 @@ class LookitGoogleCloudStorage(GoogleCloudStorage):
         return super()._normalize_name(name.lower())
 
     def url(self, name):
-        """
-        The parent implementation calls GoogleCloudStorage on every request
-        once for oauth and again to get the file. Since we're proxying requests
-        through nginx's proxy_pass to GoogleCloudStorage we don't need their url
-        nonsense or to use the actual domain or bucket name.
+        """Override for the URL getter function.
+
+        Since we're proxying requests through nginx's proxy_pass to GCS, we can avoid blob
+        creation and the signed url generation that might happen otherwise (see parent implementation for details).
         """
         name = self._normalize_name(name)
         return f"/{name.lstrip('/')}"
