caafce3fa9e8966079b8bb536d584841afe28604
Rico Rodriguez <rico.l.rodriguez@gmail.com>
2019-06-20 11:49:19 -0400

    cleaning up unused imports

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,14 +1,9 @@
 import datetime
 
-from bitfield.forms import BitFieldCheckboxSelectMultiple, BitFormField
+from bitfield.forms import BitFieldCheckboxSelectMultiple
 from django import forms
-from django.contrib.auth.forms import (
-    PasswordChangeForm,
-    UserChangeForm,
-    UserCreationForm,
-)
+from django.contrib.auth.forms import PasswordChangeForm, UserCreationForm
 from django.core.exceptions import ValidationError
-from django.forms.widgets import DateInput
 from guardian.shortcuts import assign_perm, get_objects_for_user, remove_perm
 
 from accounts.models import Child, DemographicData, User
