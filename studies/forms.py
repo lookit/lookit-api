a747b838bbeacd8f7c1803ef5a5a5e3d720770a4
Rico Rodriguez <rico.l.rodriguez@gmail.com>
2019-08-01 15:20:40 -0400

    YAPFing study forms

--- studies/forms.py
+++ studies/forms.py
@@ -27,6 +27,7 @@ class ResponseForm(ModelForm):
 
 
 class BaseStudyForm(ModelForm):
+
     def clean(self):
         cleaned_data = super().clean()
         min_age_days = self.cleaned_data.get("min_age_days")
@@ -36,30 +37,26 @@ class BaseStudyForm(ModelForm):
         max_age_months = self.cleaned_data.get("max_age_months")
         max_age_years = self.cleaned_data.get("max_age_years")
         if (min_age_years + min_age_months / 12 + min_age_days / 365) > (
-            max_age_years + max_age_months / 12 + max_age_days / 365
-        ):
+                max_age_years + max_age_months / 12 + max_age_days / 365):
             raise forms.ValidationError(
-                "The maximum age must be greater than the minimum age."
-            )
+                "The maximum age must be greater than the minimum age.")
         return cleaned_data
 
 
-STUDY_HELP_TEXT_INITIAL = """<p>After selecting a study type above, you'll be asked 
+STUDY_HELP_TEXT_INITIAL = """<p>After selecting a study type above, you'll be asked
     to fill out some study type metadata as well. This metadata is unique to the 
     study type, and provides important configurations for building your study.</p>
     <p>If you're not sure what to enter here, just leave the defaults (you can change this later).</p>
     <p>For more information on study types and their metadata, please
     <a href="https://lookit.readthedocs.io/en/develop/experimenter.html#editing-study-type">see the documentation.</a></p>"""
 
-STUDY_HELP_TEXT_EDIT = (
-    STUDY_HELP_TEXT_INITIAL
-    + """<p> Once you've filled in the 
+STUDY_HELP_TEXT_EDIT = (STUDY_HELP_TEXT_INITIAL +
+                        """<p> Once you've filled in the 
     required metadata, you'll have to build the dependencies for your experiment in order 
     to deploy it, which you can do from the detail page. However, you'll probably want 
     to see what your experiment looks like before you actually deploy it and start 
     collecting data! You can do that by clicking the "Build Preview Dependencies" button and 
-    then clicking on "See Preview" above after the build finishes.</p>"""
-)
+    then clicking on "See Preview" above after the build finishes.</p>""")
 
 
 class StudyEditForm(BaseStudyForm):
@@ -80,7 +77,8 @@ class StudyEditForm(BaseStudyForm):
             showprintmargin=False,
         ),
         required=False,
-        help_text="Add the frames of your study as well as the sequence of those frames.  This can be added later.",
+        help_text=
+        "Add the frames of your study as well as the sequence of those frames.  This can be added later.",
     )
 
     def clean_structure(self):
@@ -115,14 +113,22 @@ class StudyEditForm(BaseStudyForm):
             "structure",
         ]
         labels = {
-            "short_description": "Short Description",
-            "long_description": "Purpose",
-            "exit_url": "Exit URL",
-            "criteria": "Participant Eligibility",
-            "contact_info": "Researcher Contact Information",
-            "public": "Discoverable - Do you want this study to be publicly discoverable on Lookit once activated?",
-            "study_type": "Study Type",
-            "compensation_description": "Compensation",
+            "short_description":
+                "Short Description",
+            "long_description":
+                "Purpose",
+            "exit_url":
+                "Exit URL",
+            "criteria":
+                "Participant Eligibility",
+            "contact_info":
+                "Researcher Contact Information",
+            "public":
+                "Discoverable - Do you want this study to be publicly discoverable on Lookit once activated?",
+            "study_type":
+                "Study Type",
+            "compensation_description":
+                "Compensation",
         }
         widgets = {
             "short_description": Textarea(attrs={"rows": 2}),
@@ -135,19 +141,28 @@ class StudyEditForm(BaseStudyForm):
         }
 
         help_texts = {
-            "image": "Please keep your file size less than 1 MB",
-            "exit_url": "Specify the page where you want to send your participants after they've completed the study.",
-            "short_description": "Describe what happens during your study here. This should give families a concrete idea of what they will be doing - e.g., reading a story together and answering questions, watching a short video, playing a game about numbers.",
-            "long_description": "Explain the purpose of your study here. This should address what question this study answers AND why that is an interesting or important question, in layperson-friendly terms.",
-            "contact_info": "This should give the name of the PI for your study, and an email address where the PI or study staff can be reached with questions. Format: PIs Name (contact: youremail@lab.edu)",
-            "criteria": "Text shown to families - this is not used to actually verify eligibility.",
-            "study_type": STUDY_HELP_TEXT_EDIT,
-            "compensation_description": "Provide a description of any compensation for participation, including when and how participants will receive it and any limitations or eligibility criteria (e.g., only one gift card per participant, being in age range for study, child being visible in consent video). Please see the Terms of Use for details on allowable compensation and restrictions. If this field is left blank it will not be displayed to participants.",
+            "image":
+                "Please keep your file size less than 1 MB",
+            "exit_url":
+                "Specify the page where you want to send your participants after they've completed the study.",
+            "short_description":
+                "Describe what happens during your study here. This should give families a concrete idea of what they will be doing - e.g., reading a story together and answering questions, watching a short video, playing a game about numbers.",
+            "long_description":
+                "Explain the purpose of your study here. This should address what question this study answers AND why that is an interesting or important question, in layperson-friendly terms.",
+            "contact_info":
+                "This should give the name of the PI for your study, and an email address where the PI or study staff can be reached with questions. Format: PIs Name (contact: youremail@lab.edu)",
+            "criteria":
+                "Text shown to families - this is not used to actually verify eligibility.",
+            "study_type":
+                STUDY_HELP_TEXT_EDIT,
+            "compensation_description":
+                "Provide a description of any compensation for participation, including when and how participants will receive it and any limitations or eligibility criteria (e.g., only one gift card per participant, being in age range for study, child being visible in consent video). Please see the Terms of Use for details on allowable compensation and restrictions. If this field is left blank it will not be displayed to participants.",
         }
 
 
 # Form for creating a new study. Study type is included as part of this form.
 class StudyForm(StudyEditForm):
+
     class Meta(StudyEditForm.Meta):
         help_texts = StudyEditForm.Meta.help_texts.copy()
         help_texts["study_type"] = STUDY_HELP_TEXT_INITIAL
@@ -167,7 +182,8 @@ class StudyBuildForm(forms.ModelForm):
             showprintmargin=False,
         ),
         required=False,
-        help_text="Add the frames of your study as well as the sequence of those frames.",
+        help_text=
+        "Add the frames of your study as well as the sequence of those frames.",
     )
 
     def clean_structure(self):
