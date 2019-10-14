6d8e3fbdae4f2ff0e99ec1bf4b5dd241ac859540
Kim Scott <kimscott@mit.edu>
2019-10-14 11:06:35 -0400

    Revert "Switch to using OSF test accounts instead of staging for authentication on staging-lookit. **THIS WILL MEAN EXISTING STAGING ACCOUNTS CAN'T LOG IN**"
    This reverts commit 9275940db7c65365b059b036e60375ca1ffe0a8b.

--- osf_oauth2_adapter/apps.py
+++ osf_oauth2_adapter/apps.py
@@ -7,10 +7,10 @@ class OsfOauth2AdapterConfig(AppConfig):
     name = "osf_oauth2_adapter"
     # staging by default so people don't have to run OSF to use this.
     osf_api_url = (
-        os.environ.get("OSF_API_URL", "https://api.test.osf.io").rstrip("/") + "/"
+        os.environ.get("OSF_API_URL", "https://api.staging.osf.io").rstrip("/") + "/"
     )
     osf_accounts_url = (
-        os.environ.get("OSF_ACCOUNTS_URL", "https://accounts.test.osf.io").rstrip(
+        os.environ.get("OSF_ACCOUNTS_URL", "https://accounts.staging.osf.io").rstrip(
             "/"
         )
         + "/"
