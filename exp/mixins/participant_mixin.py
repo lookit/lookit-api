e22c112f998d1a5a747e34636f40120dbde2d2a5
Kim Scott <kimscott@mit.edu>
2020-06-14 16:21:09 -0400

    remove use of can_view_experimenter permission

--- exp/mixins/participant_mixin.py
+++ exp/mixins/participant_mixin.py
@@ -1,19 +1,13 @@
-from django.contrib.auth.mixins import (
-    PermissionRequiredMixin as DjangoPermissionRequiredMixin,
-)
 from django.db.models import Prefetch, Q
-from guardian.shortcuts import get_objects_for_user
 
 from accounts.models import Child, User
 from studies.permissions import StudyPermission
 from studies.queries import get_consented_responses_qs
 
 
-class ParticipantMixin(DjangoPermissionRequiredMixin):
+class ParticipantMixin:
     """Mixin with shared items for Participant Detail and Participant List Views"""
 
-    permission_required = "accounts.can_view_experimenter"
-    raise_exception = True
     model = User
 
     def valid_responses(self):
