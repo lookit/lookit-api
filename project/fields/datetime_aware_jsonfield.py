184e02b31ee2c4eab3bc40e99f9a02c79bd9a668
Rico Rodriguez <rrodrigu@mit.edu>
2020-06-22 17:12:06 -0400

    isort/linting + Precommit linting

--- project/fields/datetime_aware_jsonfield.py
+++ project/fields/datetime_aware_jsonfield.py
@@ -6,6 +6,7 @@ import logging
 from decimal import Decimal
 from functools import partial
 
+import ciso8601
 import pytz
 from django.contrib.postgres import lookups
 from django.contrib.postgres.fields.jsonb import JSONField
@@ -13,8 +14,6 @@ from django.contrib.postgres.forms.jsonb import JSONField as JSONFormField
 from django.core.exceptions import ValidationError
 from django.core.serializers.json import DjangoJSONEncoder
 from psycopg2.extras import Json
-
-import ciso8601
 
 
 class NaiveDatetimeException(Exception):
