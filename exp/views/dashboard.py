f411c509863291014e6d436544fe750e427989c5
Kim Scott <kimscott@mit.edu>
2020-06-24 16:22:38 -0400

    /exp should redirect to experimenter login for non-logged-in users!

--- exp/views/dashboard.py
+++ exp/views/dashboard.py
@@ -2,20 +2,20 @@ from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views import generic
 
+from exp.views import ExperimenterLoginRequiredMixin
 
-class ExperimenterDashboardView(generic.TemplateView):
+
+class ExperimenterDashboardView(ExperimenterLoginRequiredMixin, generic.TemplateView):
     """
     ExperimenterDashboard will show a customized view to each user based on the
-    role and tasks that they perform.
+    role and tasks that they perform. Redirects to experimenter login for non-logged-in users.
     """
 
     template_name = "exp/dashboard.html"
 
     def dispatch(self, request, *args, **kwargs):
-        # non-researchers, send back to web app
-        if (
-            not hasattr(request.user, "is_researcher")
-        ) or not request.user.is_researcher:
+        # logged-in non-researchers, send back to web app
+        if hasattr(request.user, "is_researcher") and not request.user.is_researcher:
             return redirect(reverse_lazy("web:home"))
         # For researchers, default to study-list
         if self.request.path.endswith("/"):
