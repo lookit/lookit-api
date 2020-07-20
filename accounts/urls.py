1ea351c7227d0a5b4df550c383b877a7f637e453
Rico Rodriguez <rrodrigu@mit.edu>
2020-07-20 15:49:52 -0400

    :bug: Put auth urls on root level
    It looks like auth urls have their success urls hardcoded to be on the root level, so that's where we have to be.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,22 +1,10 @@
 from django.conf.urls import include
-from django.contrib.auth.urls import urlpatterns as auth_urls
 from django.urls import path
-from more_itertools import locate
 
-from accounts.views import (
-    ResearcherRegistrationView,
-    TwoFactorAuthLoginView,
-    TwoFactorAuthSetupView,
-)
+from accounts.views import ResearcherRegistrationView, TwoFactorAuthSetupView
 
 app_name = "accounts"
 
-# Don't want to depend on the login always being the first view...
-# plus we REALLY should be using more_itertools :)
-login_path_index = next(locate(auth_urls, lambda patt: patt.name == "login"))
-auth_urls[login_path_index] = path(
-    "login/", TwoFactorAuthLoginView.as_view(), name="login"
-)
 
 urlpatterns = [
     path(
@@ -25,5 +13,4 @@ urlpatterns = [
         name="researcher-registration",
     ),
     path("2fa/", TwoFactorAuthSetupView.as_view(), name="2fa-setup"),
-    path("", include(auth_urls)),
 ]
