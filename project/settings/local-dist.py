6591bda6d5e408223a0428fa7da6fd86d4c01386
Rhodri Cusack <rhodricusack@gmail.com>
2020-07-22 18:25:49 +0100

    Removed local debugging line

--- project/settings/local-dist.py
+++ project/settings/local-dist.py
@@ -18,5 +18,3 @@ EMAIL_FROM_ADDRESS = "lookit.robot@some.domain"
 EMAIL_BACKEND = (
     f"django.core.mail.backends.{'console' if DEBUG else 'smtp'}.EmailBackend"
 )
-
-# SESSION_COOKIE_SECURE= False # For local debugging
