0cde537c60c0c22679bd636c633918f092196cb7
Rhodri Cusack <rhodricusack@Rhodris-iMac.local>
2020-11-16 19:13:32 +0000

    password validation i18n

--- accounts/forms.py
+++ accounts/forms.py
@@ -182,10 +182,9 @@ class ResearcherRegistrationForm(LowercaseUsernameUserCreationForm):
 
 class ParticipantSignupForm(LowercaseUsernameUserCreationForm):
     nickname = forms.CharField(required=True, max_length=255)
-
-    print(self.password1)
-    self.password1.help_text="testing stuff"
     
+    password1.help_text=_('test me here')
+
     def save(self, commit=True):
         user = super().save(commit=False)
         user.set_password(self.cleaned_data["password1"])
