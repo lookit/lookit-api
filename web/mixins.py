32ea68df3c2dcb2606340bffa58510f1b8913897
Rhodri Cusack <rhodricusack@Rhodris-iMac.local>
2020-11-16 15:24:43 +0000

    fixin auth mixin

--- web/mixins.py
+++ web/mixins.py
@@ -4,9 +4,12 @@ from django.utils.translation import get_language_from_request
 class LoginRequiredMixin(guardian.mixins.LoginRequiredMixin):
     # Customises guardian.mixins auth check, so that it redirects to [language code]/login instead of just login 
     def dispatch(self, request, *args, **kwargs):
-        print(request.path)
-        print(get_language_from_request(request, check_path=True))
-        print('Found this %s'% request.POST.get('next',''))
+        lang=get_language_from_request(request, check_path=True)
+        if lang:
+            print('Switching login URL from this %s'%self.login_url))
+            if self.login_url[:(2+len(lang))]~='/' + lang +'/':
+                self.login_url='/' + lang + self.login_url
+            print(' to this %s'%self.login_url))
         return super().dispatch(request, *args, **kwargs)
 
 
