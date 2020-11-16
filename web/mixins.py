138f336bbec50dcc8dc94557b3cfb31dbd8a6d5e
Rhodri Cusack <rhodricusack@Rhodris-iMac.local>
2020-11-16 15:14:15 +0000

    fixin auth mixin

--- web/mixins.py
+++ web/mixins.py
@@ -4,6 +4,8 @@ class LoginRequiredMixin(guardian.mixins.LoginRequiredMixin):
    
    def dispatch(self, request, *args, **kwargs):
        print(self.redirect_field_name)
+       print(args)
+       print(kwargs)
        print('Found this %s'% request.POST.get('next',''))
        return super().dispatch(request, *args, **kwargs)
    
