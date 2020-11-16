16b2223d3e9f811c0f8ed122fa581e769e32d94f
Rhodri Cusack <rhodricusack@Rhodris-iMac.local>
2020-11-16 15:20:44 +0000

    fixin auth mixin

--- web/mixins.py
+++ web/mixins.py
@@ -5,7 +5,7 @@ class LoginRequiredMixin(guardian.mixins.LoginRequiredMixin):
     # Customises guardian.mixins auth check, so that it redirects to [language code]/login instead of just login 
     def dispatch(self, request, *args, **kwargs):
         print(request.path)
-        print(get_language_from_request(request))
+        print(get_language_from_request(request, check_path=True))
         print('Found this %s'% request.POST.get('next',''))
         return super().dispatch(request, *args, **kwargs)
 
