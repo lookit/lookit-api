8bd9fb1ede1dfdf0c1df0d03447a0294366d0bc1
Kim Scott <kimscott@mit.edu>
2021-01-17 17:31:20 -0500

    simplify text to translate about child eligibility; fixes #618 but introduces a few altered strings to translate!

--- web/templatetags/web_extras.py
+++ web/templatetags/web_extras.py
@@ -1,10 +1,10 @@
 from django import template
 
-from accounts.queries import get_child_eligibility_for_study
+from accounts.queries import get_child_eligibility
 
 register = template.Library()
 
 
 @register.simple_tag
-def child_is_valid_for_study(child, study):
-    return get_child_eligibility_for_study(child, study)
+def child_is_valid_for_study_criteria_expression(child, study):
+    return get_child_eligibility(child, study.criteria_expression)
