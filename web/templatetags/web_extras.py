1a829df9a4f0d4a302dde987c00f7cd6adc72d7c
CJ Green <44074998+okaycj@users.noreply.github.com>
2021-05-25 10:22:16 -0400

    Google Tag Manager (#699)
    * Update template tag
    
    * Update templates
    
    * Add environment variable
    
    * Remove noscript variant of GTM

--- web/templatetags/web_extras.py
+++ web/templatetags/web_extras.py
@@ -1,3 +1,4 @@
+import os
 import textwrap
 from typing import Text
 
@@ -5,9 +6,18 @@ from django import template
 from django.utils.safestring import mark_safe
 
 from accounts.queries import get_child_eligibility
-from project import settings
+from project.settings import DEBUG
 
 register = template.Library()
+
+GOOGLE_TAG_MANAGER_ID = os.environ.get("GOOGLE_TAG_MANAGER_ID", "")
+
+
+def format(text: Text) -> Text:
+    if not DEBUG and GOOGLE_TAG_MANAGER_ID:
+        return mark_safe(textwrap.dedent(text))
+    else:
+        return ""
 
 
 @register.simple_tag
@@ -16,19 +26,13 @@ def child_is_valid_for_study_criteria_expression(child, study):
 
 
 @register.simple_tag
-def google_analytics() -> Text:
-    if not settings.DEBUG:
-        ga_js = textwrap.dedent(
-            """
-                <script async src="https://www.googletagmanager.com/gtag/js?id=UA-163886699-1"></script>
-                <script>
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'UA-163886699-1');
-                </script>
-            """
-        )
-        return mark_safe(ga_js)
-    else:
-        return ""
+def google_tag_manager() -> Text:
+    return format(
+        f"""
+    <script>(function(w,d,s,l,i){{w[l]=w[l]||[];w[l].push({{'gtm.start':
+    new Date().getTime(),event:'gtm.js'}});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    }})(window,document,'script','dataLayer','{GOOGLE_TAG_MANAGER_ID}');</script>
+    """
+    )
