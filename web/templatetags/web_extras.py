a0058811357374f11ddd0148df7bb4aefad2fcb0
Christopher J. Green <okaycj@mit.edu>
2021-06-17 17:01:30 -0400

    Add template tag to highlight current page in nav

--- web/templatetags/web_extras.py
+++ web/templatetags/web_extras.py
@@ -3,7 +3,9 @@ import textwrap
 from typing import Text
 
 from django import template
+from django.urls.base import reverse
 from django.utils.safestring import mark_safe
+from django.utils.translation import gettext as _
 
 from accounts.queries import get_child_eligibility
 from project.settings import DEBUG
@@ -36,3 +38,14 @@ def google_tag_manager() -> Text:
     }})(window,document,'script','dataLayer','{GOOGLE_TAG_MANAGER_ID}');</script>
     """
     )
+
+
+@register.simple_tag
+def nav_item(request, url_name, text):
+    li_class = ""
+    url = reverse(url_name)
+
+    if request.path == url:
+        li_class = "active"
+
+    return mark_safe(f'<li class="{li_class}"><a href="{url}">{_(text)}</a></li>')
