a475345c55b6e46d1ceb1a2d8bf2f124b8b9af6e
Christopher J. Green <okaycj@mit.edu>
2021-07-15 17:19:48 -0400

    Add support for checkboxes

--- exp/views/mixins.py
+++ exp/views/mixins.py
@@ -142,9 +142,12 @@ class StudyTypeMixin:
         metadata = {}
 
         for type_field in type_fields:
-            metadata[type_field["name"]] = self.request.POST.get(
-                type_field["name"], None
-            )
+            if type_field["input_type"] == "checkbox":
+                metadata[type_field["name"]] = type_field["name"] in self.request.POST
+            else:
+                metadata[type_field["name"]] = self.request.POST.get(
+                    type_field["name"], None
+                )
 
         return metadata
 
