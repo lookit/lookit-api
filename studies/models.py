c1419f23013998e4858978ef07da1e98adfd93bd
CJ Green <44074998+okaycj@users.noreply.github.com>
2021-10-14 08:50:17 -0400

    update str method for ConsentRuling (#825)

--- studies/models.py
+++ studies/models.py
@@ -1344,9 +1344,4 @@ class ConsentRuling(models.Model):
         index_together = (("response", "action"), ("response", "arbiter"))
 
     def __str__(self):
-
-        if self.arbiter:
-            arbitor_name = self.arbiter.get_short_name()
-        else:
-            arbitor_name = None
-        return f"<{arbitor_name}: {self.action} {self.response} @ {self.created_at:%c}>"
+        return f"<{self.arbiter.get_short_name()}: {self.action} {self.response} @ {self.created_at:%c}>"
