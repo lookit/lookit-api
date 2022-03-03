0f63138e6a6ad92fb3f24994000eb77b38d63bc9
CJ Green <44074998+okaycj@users.noreply.github.com>
2022-03-03 10:29:44 -0500

    Remove debug toolbar
    Generally, this toolbar isn't used by me for debuging or performance
    issues.  Instead, this toolbar causes the development environment to
    load significantly slower and gets pinged for accessibility issues.
    This bar is not used in production.

--- project/urls.py
+++ project/urls.py
@@ -54,10 +54,4 @@ urlpatterns = i18n_patterns(
 )
 
 if settings.DEBUG:
-    import debug_toolbar
-
-    urlpatterns = (
-        [path("__debug__/", include(debug_toolbar.urls))]
-        + urlpatterns
-        + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    )
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
