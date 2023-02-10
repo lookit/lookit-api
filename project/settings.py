3faa523d7a83cd3f79fe955d06075f54161db641
Becky Gilbert <beckyannegilbert@gmail.com>
2023-02-09 16:26:29 -0800

    remove project/_static and move files to web/static

--- project/settings.py
+++ project/settings.py
@@ -301,7 +301,6 @@ else:
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "_static"),)
 
 EMAIL_FROM_ADDRESS = os.environ.get("EMAIL_FROM_ADDRESS", "lookit.robot@some.domain")
 DEFAULT_FROM_EMAIL = EMAIL_FROM_ADDRESS  # for Django-generated password reset emails
