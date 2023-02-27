0f7b4ba009df7a77984e2da2b3bea39cd6b9f944
CJ Green <44074998+okaycj@users.noreply.github.com>
2023-02-27 11:50:28 -0500

    Initial BS5 Update to exp study detail view

--- studies/static/studies/js/study-detail.js
+++ studies/static/studies/js/study-detail.js
@@ -1,4 +1,17 @@
-$.fn.editable.defaults.mode = 'inline';
+// $.fn.editable.defaults.mode = 'inline';
+
+$.fn.editableform.buttons = `
+<button type="submit" class="btn btn-primary btn-sm editable-submit">ok</button>
+<button type="button" class="btn btn-secondary btn-sm editable-submit">cancel</button>
+`
+
+$.fn.editableform.template = `
+<form class="form-inline editableform">
+<div class="control-group">
+     <div><div class="editable-input"></div><div class="editable-buttons"></div></div>
+     <div class="editable-error-block"></div>
+</div> 
+</form>`
 
 function cloneStudy() {
     document.getElementById('cloneForm').submit()
@@ -40,26 +53,26 @@ function onStateSelect(stateListItem) {
     $declarationsForm.empty();
     if (trigger in declarations) {
         $declarationsForm.append(`
-            <p>
-                Please note here any elements of your study that require additional review (see Terms of Use):
-            </p>
-        `);
+    < p >
+    Please note here any elements of your study that require additional review(see Terms of Use):
+            </p >
+    `);
         for (let key in declarations[trigger]) {
             $declarationsForm.append(`
-                <div class="checkbox">
-                    <label>
-                        <input type="checkbox" value="" name="${key}"/>
-                        ${declarations[trigger][key]}
-                    </label>
-                </div>
-        `)
+    < div class="checkbox" >
+        <label>
+            <input type="checkbox" value="" name="${key}" />
+            ${declarations[trigger][key]}
+        </label>
+                </div >
+    `)
         }
         $declarationsForm.append(`
-        <p>
-            If you checked any of the boxes above, please describe below:
-        </p>
-        <textarea class="form-control" rows="5" name="issues_description" placeholder="Describe declarations here"></textarea>
-    `);
+    < p >
+    If you checked any of the boxes above, please describe below:
+        </p >
+    <textarea class="form-control" rows="5" name="issues_description" placeholder="Describe declarations here"></textarea>
+`);
     }
 }
 
@@ -74,9 +87,6 @@ $(document).ready(function () {
         previewLink.value = origin + previewLink.value;
     }
 
-    if ("{{ match }}" !== '') {
-        document.getElementById("search-organization").value = "{{ match }}";
-    }
     $('.researcher_permissions').editable({
         source: [
             { value: 'study_preview', text: 'Preview' },
@@ -86,29 +96,41 @@ $(document).ready(function () {
             { value: 'study_researcher', text: 'Researcher' },
             { value: 'study_manager', text: 'Manager' },
             { value: 'study_admin', text: 'Admin' },
-        ],
-        error: function (response, _newValue) {
-            // If removing own permissions, direct to study detail page.
-            if (response.status === 403) {
-                window.location = window.location.href.replace('edit/', '');
-            }
-        },
-        success: function (_response, newValue) {
-            // On success, populate the success message with the permissions the user was given
-            // and reveal the permission edit alert message.
-            // This is necessary b/c we're using x-editable here. Page is not reloaded.
-            $('#add-researcher-messages').hide()
-            $('#permission-edit-text').append(' given ' + newValue + ' permissions for this study.');
-            $('.disabledPermissionDisplay').hide()
-            $('.permissionDisplay').show();
-            // Shows success message
-            $('#permission-edit').show();
-        }
-    }).on('click', function (event) {
-        // When clicking on a researcher, prepopulate success message with researcher name.
-        $('#permission-edit').hide();
-        $('#permission-edit-text').text($(event.currentTarget).attr('data-id'));
+        ]
     });
+
+    // $('.researcher_permissions').editable({
+    //     source: [
+    //         { value: 'study_preview', text: 'Preview' },
+    //         { value: 'study_design', text: 'Design' },
+    //         { value: 'study_analysis', text: 'Analysis' },
+    //         { value: 'study_submission_processor', text: 'Submission processor' },
+    //         { value: 'study_researcher', text: 'Researcher' },
+    //         { value: 'study_manager', text: 'Manager' },
+    //         { value: 'study_admin', text: 'Admin' },
+    //     ],
+    //     error: function (response, _newValue) {
+    //         // If removing own permissions, direct to study detail page.
+    //         if (response.status === 403) {
+    //             window.location = window.location.href.replace('edit/', '');
+    //         }
+    //     },
+    //     success: function (_response, newValue) {
+    //         // On success, populate the success message with the permissions the user was given
+    //         // and reveal the permission edit alert message.
+    //         // This is necessary b/c we're using x-editable here. Page is not reloaded.
+    //         // $('#add-researcher-messages').hide()
+    //         // $('#permission-edit-text').append(' given ' + newValue + ' permissions for this study.');
+    //         // $('.disabledPermissionDisplay').hide()
+    //         // $('.permissionDisplay').show();
+    //         // // Shows success message
+    //         // $('#permission-edit').show();
+    //     }
+    // }).on('click', function (event) {
+    //     // When clicking on a researcher, prepopulate success message with researcher name.
+    //     // $('#permission-edit').hide();
+    //     // $('#permission-edit-text').text($(event.currentTarget).attr('data-id'));
+    // });
 
 
     new Clipboard('#copy-link-button'); // NOSONAR
@@ -122,9 +144,10 @@ $(document).ready(function () {
     $('.question-icon').tooltip({
         placement: "top",
     });
-    $('body').on('hidden.bs.tooltip', function (e) {
-        $(e.target).data("bs.tooltip").inState.click = false;
-    });
+
+    // $('body').on('hidden.bs.tooltip', function (e) {
+    //     $(e.target).data("bs.tooltip").inState.click = false;
+    // });
 
     removeTooltip = function () {
         $('[data-toggle="tooltip"]').tooltip('hide');
