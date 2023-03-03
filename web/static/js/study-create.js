881284328b8caf90b834e7ba258f69ea06031bea
Becky Gilbert <beckyannegilbert@gmail.com>
2023-03-02 21:57:34 -0800

    refactor to remove JS and CSS from templates

---
+++ web/static/js/study-create.js
@@ -0,0 +1,14 @@
+/* Fix for ace-editor. It thinks it's in the admin only */
+(function (){
+    if (window.jQuery !== undefined) {
+        window.django = {
+            'jQuery': window.jQuery
+        };
+    }
+})();
+
+$(document).ready(function() {
+    $('form').submit(function() {
+        $('#create-study-button').prop("disabled", "disabled");
+    });
+});
--- web/static/js/study-edit.js
+++ web/static/js/study-edit.js
@@ -1,22 +1,20 @@
 /* Fix for ace-editor. It thinks it's in the admin only */
-(function () {
-  if (window.jQuery !== undefined) {
-    window.django = {
-      jQuery: window.jQuery,
-    };
-  }
+(function (){
+    if (window.jQuery !== undefined) {
+        window.django = {
+            'jQuery': window.jQuery
+        };
+    }
 })();
 
-$(document).ready(function () {
+$(document).ready(function() {
     $("#invalidate-build-warning").hide();
     $("#save-button").click(function () {
         $("#save-study-confirmation-body").show();
         var runner_type_changed = $("#id_study_type").data("previous") != $("#id_study_type").val();
-        console.log('runner type changed: ', runner_type_changed);
         $("#study-type-metadata div.metadata-key").each(function (index, element) {
             runner_type_changed = runner_type_changed || $(element).data("previous") != $("input", element).val();
         });
-        console.log('runner type changed: ', runner_type_changed);
         if (runner_type_changed) {
             $("#invalidate-build-warning").show();
         } else {
