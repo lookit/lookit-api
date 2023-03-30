bef68375ba213940300a5e1279caaf7153ba19a8
CJ Green <44074998+okaycj@users.noreply.github.com>
2023-03-30 10:27:32 -0400

    Shorten password requirements to 10 chars

--- project/settings.py
+++ project/settings.py
@@ -360,3 +360,21 @@ LOCALE_PATHS = (os.path.join(BASE_DIR, "../locale"),)
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
 BOOTSTRAP5 = {"css_url": ("/static/custom_bootstrap5.css")}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "OPTIONS": {
+            "min_length": 10,
+        },
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
