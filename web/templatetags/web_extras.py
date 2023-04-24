bd14d46da045db7d29c2c7840c38704d77b2ccbf
CJ Green <44074998+okaycj@users.noreply.github.com>
2023-04-24 11:52:54 -0400

    Replace template function with new css class (#1177)

--- web/templatetags/web_extras.py
+++ web/templatetags/web_extras.py
@@ -299,8 +299,3 @@ class FormButtonsNode(template.Node):
     def render(self, context):
         rendered_buttons = (n.render(context) for n in self.nodelist)
         return f'<div class="d-flex justify-content-end gap-1">{"".join(rendered_buttons)}</div>'
-
-
-@register.simple_tag
-def empty_text(text):
-    return mark_safe(f'<div class="my-4 mx-auto text-center fst-italic">{text}</div>')
