bce2e2c6956162d7452c7a2010d4ee6d4824815e
Becky Gilbert <beckyannegilbert@gmail.com>
2023-07-05 16:02:10 -0700

    only check POST requests signatures (non-POST requests should be rejected due to method rather than signature)

--- api/permissions.py
+++ api/permissions.py
@@ -48,37 +48,43 @@ class VideoFromS3Permissions:
     def has_permission(self, request, view):
         """
         Only allow the creation of a new video via the API if request is signed and signature is verified.
+        Only check the signature on POST requests (other methods will be rejected via the view).
         """
-        try:
-            signature_received = request.META["HTTP_X_AWS_LAMBDA_HMAC_SIG"]
-        except KeyError:
+        if request.method == "POST":
             try:
-                signature_received = request.META["headers"]["X_AWS_LAMBDA_HMAC_SIG"]
+                signature_received = request.META["HTTP_X_AWS_LAMBDA_HMAC_SIG"]
             except KeyError:
-                # results in 401 unauthorized response
-                raise exceptions.AuthenticationFailed(
-                    f"No HMAC signature found in request header."
-                )
+                try:
+                    signature_received = request.META["headers"][
+                        "X_AWS_LAMBDA_HMAC_SIG"
+                    ]
+                except KeyError:
+                    # results in 401 unauthorized response
+                    raise exceptions.AuthenticationFailed(
+                        f"No HMAC signature found in request header."
+                    )
 
-        # calculate signature to compare with the one sent
-        key = bytes(AWS_LAMBDA_SECRET_ACCESS_KEY, "UTF-8")
-        # remove study/response relationships from request data dict to match the content/format sent by client
-        request_data = request.data.copy()
-        try:
-            request_data.pop("study")
-        except KeyError:
-            raise exceptions.ParseError("Missing required relationship: study")
+            # calculate signature to compare with the one sent
+            key = bytes(AWS_LAMBDA_SECRET_ACCESS_KEY, "UTF-8")
+            # remove study/response relationships from request data dict to match the content/format sent by client
+            request_data = request.data.copy()
+            try:
+                request_data.pop("study")
+            except KeyError:
+                raise exceptions.ParseError("Missing required relationship: study")
 
-        try:
-            request_data.pop("response")
-        except KeyError:
-            raise exceptions.ParseError("Missing required relationship: response")
+            try:
+                request_data.pop("response")
+            except KeyError:
+                raise exceptions.ParseError("Missing required relationship: response")
 
-        message = bytes(json.dumps(request_data, separators=(",", ":")), "UTF-8")
-        signature_calculated = hmac.new(key, message, hashlib.sha256).hexdigest()
+            message = bytes(json.dumps(request_data, separators=(",", ":")), "UTF-8")
+            signature_calculated = hmac.new(key, message, hashlib.sha256).hexdigest()
 
-        # false results in 401 unauthorized response
-        if not signature_received == signature_calculated:
-            raise exceptions.AuthenticationFailed(f"HMAC signatures do not match.")
+            # false results in 401 unauthorized response
+            if not signature_received == signature_calculated:
+                raise exceptions.AuthenticationFailed(f"HMAC signatures do not match.")
+            else:
+                return True
         else:
             return True
