c17ebb96ade2135fc1855d707d5af4994ce42661
CJ Green <44074998+okaycj@users.noreply.github.com>
2023-07-26 13:27:37 -0400

    Add runner config edit html

--- web/static/js/study-fields.js
+++ web/static/js/study-fields.js
@@ -1,62 +1,3 @@
-// Show the generator function field only if use_generator is checked.
-function updateGeneratorDisplay() {
-    if ($('#id_use_generator:checked').length) {
-        document.querySelector('#generator-container').classList.remove('d-none');
-    } else {
-        document.querySelector('#generator-container').classList.add('d-none');
-    }
-}
-
-// Display an error if generator is not a valid JS function. Returns 0 if no errors, 1 if errors.
-function validateGenerator(code) {
-
-    const $errorToDisplay = $('<div id="clientside-generator-validation-message" class="form-text"></div>');
-    const $generatorField = $('#id_generator').closest('.mb-4');
-    $generatorField.removeClass('has-error');
-    $('#clientside-generator-validation-message').remove();
-
-    try {
-        Function(code)();  //NOSONAR
-        try {
-            const generatorFn = Function('return ' + code)(); //NOSONAR
-            if (typeof generatorFn !== 'function') {
-                throw new Error();
-            }
-        } catch (error) {
-            $errorToDisplay
-                .text('Warning: Generator function does not evaluate to a single function. Generator will be disabled if this value is saved.')
-                .insertBefore($generatorField.children().last());
-            $generatorField.addClass('has-error');
-            return 1;
-        }
-    } catch (error) {
-        $errorToDisplay
-            .text('Warning: Invalid Javascript. Generator will be disabled if this value is saved.')
-            .insertBefore($generatorField.children().last());
-        $generatorField.addClass('has-error');
-        return 1;
-    }
-    return 0;
-}
-
-// Display an error if structure is not a valid JSON string. Returns 0 if no errors, 1 if errors.
-function validateStructure(code) {
-    const $errorToDisplay = $('<div id="clientside-protocol-validation-message" class="form-text"></div>');
-    const $protocolField = $('#id_structure').closest('.mb-4');
-    $protocolField.removeClass('has-error');
-    $('#clientside-protocol-validation-message').remove();
-    try {
-        JSON.parse(code);
-    } catch (error) {
-        $errorToDisplay
-            .text('Warning: Invalid JSON.')
-            .insertBefore($protocolField.children().last());
-        $protocolField.addClass('has-error');
-        return 1;
-    }
-    return 0;
-}
-
 // Update a read-only field to display the calculated max age in days
 function updateMaxAgeDaysDisplay() {
     $("#max_age_in_days_val").text(Number($("#id_max_age_years").val()) * 365 + Number($("#id_max_age_months").val()) * 30 + Number($("#id_max_age_days").val()))
@@ -67,126 +8,22 @@ function updateMinAgeDaysDisplay() {
     $("#min_age_in_days_val").text(Number($("#id_min_age_years").val()) * 365 + Number($("#id_min_age_months").val()) * 30 + Number($("#id_min_age_days").val()))
 }
 
-function setExternal() {
-    // hide frame player fields through css                
-    document
-        .querySelectorAll('#type-metadata-1, #structure-container, #generator-container, .use-generator.form-text')
-        .forEach(e => e.classList.add('d-none'))
-    // disable frame player metadata fields
-    document
-        .querySelectorAll("#type-metadata-1 input")
-        .forEach(e => e.disabled = true)
-    // enable external fields
-    document
-        .querySelectorAll("#type-metadata-2 input")
-        .forEach(e => e.disabled = false)
-    document.querySelector('#id_use_generator').closest('.form-group').classList.add('d-none')
-    document.querySelector('#type-metadata-2').classList.remove('d-none')
-}
 
-function setFramePlayer() {
-    // hide external fields through css
-    document
-        .querySelectorAll('#type-metadata-1, #structure-container, #generator-container, .use-generator.form-text')
-        .forEach(e => e.classList.remove('d-none'))
-    // enable frame player metadata fields
-    document
-        .querySelectorAll("#type-metadata-1 input")
-        .forEach(e => e.disabled = false)
-    // disable external fields
-    document
-        .querySelectorAll("#type-metadata-2 input")
-        .forEach(e => e.disabled = true)
-    document.querySelector('#id_use_generator').closest('.form-group').classList.remove('d-none')
-    document.querySelector('#type-metadata-2').classList.add('d-none')
 
-    updateGeneratorDisplay();
-}
 
-function updateStudyType(externalCheckbox) {
-    externalCheckbox.checked ? setExternal() : setFramePlayer()
-    updateScheduled()
-}
 
-function updateScheduled() {
-    const external = document.querySelector('#id_external')
-    const scheduled = document.querySelector('#id_scheduled')
-    scheduled.disabled = !external.checked
-}
 
-function updateScheduling(scheduledCheckBox) {
-    const scheduling = document.querySelector("#id_scheduling").closest('.mb-4')
-    if (!scheduledCheckBox.disabled && scheduledCheckBox.checked) {
-        scheduling.classList.remove("d-none")
-    } else {
-        scheduling.classList.add("d-none")
-    }
-}
+
+
+
+
 
 $(document).ready(function () {
-    // Do initial validation of structure, generator.
-    validateStructure($('#id_structure').val());
-    validateGenerator($('#id_generator').val());
-
-    // Update study form based on study type
-    const externalCheckbox = document.querySelector('#id_external')
-    externalCheckbox.addEventListener('click', () => updateStudyType(externalCheckbox))
-    updateStudyType(externalCheckbox)
-
-    // When use_generator field changes, update whether generator field is displayed
-    $('#id_use_generator').on('change', function () {
-        updateGeneratorDisplay();
-    });
-
-    // Validate generator function upon closing its editor
-    $("#generator-container .ace-overlay .save, #generator-container .ace-overlay .cancel").bind("click", function () {
-        // data field 'editor' bound to editor but only while editor is open
-        const code = $('#generator-container .ace-overlay').data('editor').getValue();
-        validateGenerator(code);
-    });
-
-    // Validate structure function upon closing its editor
-    $("#structure-container .ace-overlay .save, #generator-container .ace-overlay .cancel").bind("click", function () {
-        // data field 'editor' bound to editor but only while editor is open
-        const code = $('#structure-container .ace-overlay').data('editor').getValue();
-        validateStructure(code);
-    });
-
-    // Use ctrl-S to close editor from either editor.
-    $(".ace-overlay .edit").bind("click", function () {
-        const $aceOverlay = $(this).closest('.ace-overlay')
-        const editor = $aceOverlay.data('editor');
-        editor.commands.addCommand({
-            name: 'save',
-            bindKey: { win: 'Ctrl-S', mac: 'Command-S' },
-            exec: function () {
-                $aceOverlay.find('.save').click();
-                // Could also consider triggering save button on broader form
-            },
-            readOnly: false // should not apply in readOnly mode
-        });
-    });
-
-    // Upon submit, unset use generator if generator function is invalid
-    $("#create-study-button, #save-button").bind("click", function () {
-        if (validateGenerator($('#id_generator').val())) {
-            const $useGeneratorCheckbox = $('#id_use_generator');
-            $useGeneratorCheckbox.prop("checked", false);
-            $useGeneratorCheckbox[0].value = false;
-        }
-    });
-
     // Calculate the min/max age in day(s) upon page load & updates
     $("#id_min_age_years, #id_min_age_months, #id_min_age_days").change(updateMinAgeDaysDisplay);
     $("#id_max_age_years, #id_max_age_months, #id_max_age_days").change(updateMaxAgeDaysDisplay);
     updateMinAgeDaysDisplay();
     updateMaxAgeDaysDisplay();
-
-    const scheduledCheckBox = document.querySelector("#id_scheduled")
-    scheduledCheckBox.addEventListener('click', () => {
-        updateScheduling(scheduledCheckBox)
-    })
-    updateScheduling(scheduledCheckBox)
 
     /*
         Priority current value
