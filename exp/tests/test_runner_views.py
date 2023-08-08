00719b07f4793028d0e69c083d967ffbfd383333
CJ Green <44074998+okaycj@users.noreply.github.com>
2023-08-08 16:12:13 -0400

    Add runner view tests

---
+++ exp/tests/test_runner_views.py
@@ -0,0 +1,120 @@
+from http import HTTPStatus
+
+from django.test import Client, TestCase
+from django.urls import reverse
+from django_dynamic_fixture import G
+from guardian.shortcuts import assign_perm
+
+from accounts.backends import TWO_FACTOR_AUTH_SESSION_KEY
+from accounts.models import User
+from project import settings
+from studies.forms import ScheduledChoice
+from studies.models import Lab, Study
+from studies.permissions import StudyPermission
+
+
+class Force2FAClient(Client):
+    @property
+    def session(self):
+        _session = super().session
+        _session[TWO_FACTOR_AUTH_SESSION_KEY] = True
+        return _session
+
+
+class ExternalDetailsViewsTestCase(TestCase):
+    def setUp(self):
+        self.client = Force2FAClient()
+
+    def test_external_details_view(self):
+        user = G(User, is_active=True, is_researcher=True)
+        lab = G(Lab)
+        study = G(Study, creator=user, lab=lab, study_type=2)
+        metadata = {
+            "url": "https://mit.edu",
+            "scheduled": ScheduledChoice.scheduled.value == "Scheduled",
+            "scheduling": "",
+            "study_platform": "",
+            "other_scheduling": "",
+            "other_study_platform": "",
+        }
+
+        assign_perm(StudyPermission.WRITE_STUDY_DETAILS.codename, user, study)
+        assign_perm(StudyPermission.READ_STUDY_DETAILS.codename, user, study)
+
+        self.client.force_login(user=user)
+        response = self.client.post(
+            reverse("exp:external-study-details", kwargs={"pk": study.id}),
+            {"scheduled": ScheduledChoice.scheduled.value, "url": metadata["url"]},
+            follow=True,
+        )
+
+        if "form" in response.context:
+            self.assertEqual(response.context_data["form"].errors, {})
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(Study.objects.get(id=study.id).metadata, metadata)
+
+    def test_efp_details_view(self):
+        user = G(User, is_active=True, is_researcher=True)
+        lab = G(Lab)
+        study = G(Study, creator=user, lab=lab, study_type=1)
+        metadata = {
+            "player_repo_url": settings.EMBER_EXP_PLAYER_REPO,
+            "last_known_player_sha": "862604874f7eeff8c9d72adcb8914b21bfb5427e",
+        }
+
+        assign_perm(StudyPermission.WRITE_STUDY_DETAILS.codename, user, study)
+        assign_perm(StudyPermission.READ_STUDY_DETAILS.codename, user, study)
+
+        self.client.force_login(user=user)
+        response = self.client.post(
+            reverse("exp:efp-study-details", kwargs={"pk": study.id}),
+            {
+                "structure": "{}",
+                "player_repo_url": metadata["player_repo_url"],
+                "last_known_player_sha": metadata["last_known_player_sha"],
+            },
+            follow=True,
+        )
+
+        if "form" in response.context:
+            self.assertEqual(response.context_data["form"].errors, {})
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(Study.objects.get(id=study.id).metadata, metadata)
+
+    def test_study_details_redirect(self):
+        user = G(User, is_active=True, is_researcher=True)
+        lab = G(Lab)
+        efp = G(Study, creator=user, lab=lab, study_type=1)
+        external = G(Study, creator=user, lab=lab, study_type=2)
+
+        assign_perm(StudyPermission.WRITE_STUDY_DETAILS.codename, user, efp)
+        assign_perm(StudyPermission.READ_STUDY_DETAILS.codename, user, efp)
+        assign_perm(StudyPermission.WRITE_STUDY_DETAILS.codename, user, external)
+        assign_perm(StudyPermission.READ_STUDY_DETAILS.codename, user, external)
+
+        self.client.force_login(user)
+        response = self.client.get(
+            reverse("exp:study-details", kwargs={"pk": efp.id}), follow=True
+        )
+        self.assertEqual(
+            response.redirect_chain,
+            [
+                (
+                    reverse("exp:efp-study-details", kwargs={"pk": efp.id}),
+                    HTTPStatus.FOUND,
+                )
+            ],
+        )
+
+        response = self.client.get(
+            reverse("exp:study-details", kwargs={"pk": external.id}), follow=True
+        )
+        self.assertEqual(
+            response.redirect_chain,
+            [
+                (
+                    reverse("exp:external-study-details", kwargs={"pk": external.id}),
+                    HTTPStatus.FOUND,
+                )
+            ],
+        )
