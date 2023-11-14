9f93e317eaaa640440fe67fda1221003fb11ea18
CJ Green <44074998+okaycj@users.noreply.github.com>
2023-11-14 12:06:38 -0500

    Remove existing error list before new one

--- web/static/js/efp-runner.js
+++ web/static/js/efp-runner.js
@@ -142,11 +142,12 @@ function validateGenerator(event) {
     const generator = document.querySelector('#id_generator');
     const use_generator = document.querySelector('#id_use_generator');
 
+    // Remove existing error list
+    document.querySelector('.js-validation')?.remove();
+
     if (use_generator.checked) {
-        if (JSHINT(generator.value, jshint_config)) {
-            console.log('all good here....')
-            this.submit();
-        } else {
+        // if jshint comes back with errors (false)
+        if (!JSHINT(generator.value, jshint_config)) {
             const breadcrumb = document.querySelector('nav.breadcrumb');
             const errorsList = document.createElement('ul')
             const errorsMsg = document.createElement('div');
@@ -156,24 +157,23 @@ function validateGenerator(event) {
             window.scrollTo(0, 0);
 
             // Errors are red
-            errorsList.classList.add('text-danger');
+            errorsList.classList.add('text-danger', 'js-validation');
 
-            // Error message is bold
+            // Error message has some space around it
             errorsMsg.classList.add('my-3');
 
             // Add error message
             errorsMsg.appendChild(document.createTextNode('Generator javascript seems to be invalid.  Please edit and save again. If you reload this page, all changes will be lost.'))
             errorsList.appendChild(errorsMsg);
-            // errorsHeader = 'Generator javascript seems to be invalid.  Please edit and save again. If you reload this page, all changes will be lost.';
-            // errorsBlock.appendChild(errorsHeader);
-            // errorsBlock.appendChild(errorsList);
 
+            // Add errors to the list
             JSHINT.errors.map(({ line, reason }) => {
                 const errorEl = document.createElement('ol');
                 errorEl.innerHTML = `${line}: ${reason}`;
                 errorsList.appendChild(errorEl);
             })
 
+            // Add list to page after breadcrumb
             breadcrumb.after(errorsList);
         }
     }
