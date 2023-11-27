80db01f520a6dc6775152f224495a1d001caba02
CJ Green <44074998+okaycj@users.noreply.github.com>
2023-11-27 12:10:33 -0500

    redirect to study exit url

--- web/static/js/lookit-jspsych.js
+++ web/static/js/lookit-jspsych.js
@@ -18,7 +18,7 @@ async function get(url) {
         url,
         {
             method: 'GET',
-            mode: 'same-origin' // Do not send CSRF token to another domain.
+            mode: 'same-origin'
         }
     );
 
@@ -48,7 +48,7 @@ async function patch(url, data) {
     return await response.json();
 }
 
-function on_data_update(url, id, userFunc) {
+function on_data_update(responseApiUrl, id, userFunc) {
     /**
      * Function that returns a function to be used in place of jsPsych's option 
      * "on_data_update".  "userFunc" should be the user's implementation of 
@@ -57,9 +57,9 @@ function on_data_update(url, id, userFunc) {
      * current data point.
      */
     return async function (data) {
-        const { data: { attributes: { exp_data } } } = await get(url);
+        const { data: { attributes: { exp_data } } } = await get(responseApiUrl);
 
-        await patch(url, {
+        await patch(responseApiUrl, {
             id, type: "responses", attributes: {
                 exp_data: [...exp_data, data]
             }
@@ -67,22 +67,23 @@ function on_data_update(url, id, userFunc) {
 
         // Don't call the function if not defined by user.
         if (typeof userFunc === 'function') {
-            userFunc(data)
-        };
-    }
+            userFunc(data);
+        }
+    };
 }
 
 
-function on_finish(url, id, userFunc) {
+function on_finish(responseApiUrl, id, exitUrl, userFunc) {
     /**
      * Function that returns a function to be used in place of jsPsych's option 
      * "on_finish".  "userFunc" should be the user's implementation of 
      * "on_finish".  Since this is point where the experiment has ended, the 
-     * function wil set completed to true and overwrites all experiment data 
-     * with the full set of collected data.
+     * function will set "completed" to true and overwrites all experiment data 
+     * with the full set of collected data.  Once the user function has been 
+     * ran, this will redirect to the study's exit url.
      */
     return async function (data) {
-        await patch(url, {
+        await patch(responseApiUrl, {
             id, type: "responses", attributes: {
                 exp_data: data.trials,
                 completed: true
@@ -92,27 +93,29 @@ function on_finish(url, id, userFunc) {
         // Don't call the function if not defined by user.
         if (typeof userFunc === 'function') {
             userFunc(data);
-        };
-    }
+        }
+
+        window.location.replace(exitUrl);
+    };
 }
 
-export default function (url, responseUuid) {
+export default function (responseApiUrl, responseUuid, exitUrl) {
     /**
      * Function that returns a function to replace jsPysch's initJsPsych.
      */
     return function (opts) {
         const jsPsych = origInitJsPsych({
             ...opts,
-            on_data_update: on_data_update(url, responseUuid, opts.on_data_update),
-            on_finish: on_finish(url, responseUuid, opts.on_finish)
+            on_data_update: on_data_update(responseApiUrl, responseUuid, opts.on_data_update),
+            on_finish: on_finish(responseApiUrl, responseUuid, exitUrl, opts.on_finish)
         });
         const origJsPsychRun = jsPsych.run;
 
         jsPsych.run = async function (timeline) {
             // check timeline here...
             return origJsPsychRun(timeline);
-        }
+        };
 
         return jsPsych;
-    }
+    };
 }
