b3d43b26ebd3eacee3c2f234f793983751edb5a9
CJ Green <44074998+okaycj@users.noreply.github.com>
2024-02-15 11:34:28 -0500

    Change linter

--- web/static/js/js-validation.js
+++ web/static/js/js-validation.js
@@ -1,26 +1,22 @@
 function jsValidation(event, generator) {
-    const jshint_config = { "esversion": 6, "sub": true };
     const jsValidation = document.querySelector('.js-validation');
+    try {
+        Babel.transform(generator.value, { presets: ["env"] });
+        jsValidation.classList.add('d-none');
+    } catch ({ message }) {
+        // Remove existing errors
+        document.querySelectorAll('.js-validation pre').forEach(el => el.remove());
 
-    // if jshint comes back with errors (false)
-    if (!JSHINT(generator.value, jshint_config)) {
         // Show errors element
         jsValidation.classList.remove('d-none');
-
-        // Remove existing errors
-        document.querySelectorAll('.js-validation ol').forEach(el => el.remove());
 
         // Don't submit the form and scroll to top of page.
         event.preventDefault();
         window.scrollTo(0, 0);
 
-        // Add errors to the list
-        JSHINT.errors.map(({ line, reason }) => {
-            const errorEl = document.createElement('ol');
-            errorEl.innerHTML = `${line}: ${reason}`;
-            jsValidation.appendChild(errorEl);
-        });
-    } else {
-        jsValidation.classList.add('d-none');
+        // Add error message
+        const errorEl = document.createElement('pre');
+        errorEl.innerHTML = message;
+        jsValidation.appendChild(errorEl);
     }
 }
