d3888a114967cf3f1952493a7f3086ab5208803e
CJ Green <44074998+okaycj@users.noreply.github.com>
2025-03-06 14:14:51 -0500

    Add show/hide columns functionality (#1520)
    * Add show/hide columns functionality
    
    * Add "Time Elapsed" show/hide

--- web/static/js/study-responses.js
+++ web/static/js/study-responses.js
@@ -41,15 +41,37 @@ function showResponse(index) {
     $('#response-summary-' + index).show();
 }
 
+function showHideColumns() {
+    // Elements containing links 
+    const show_hide_columns = [{ col: 4, name: "Date" }, { col: 5, name: "Time Elapsed" }]
+    const hide_show_elements = show_hide_columns.map(el => {
+        return `<a href class="toggle-vis" data-column="${el.col}">${el.name}</a>`
+    })
+    document.querySelector('.show-hide-cols').innerHTML = `<div class="text-center py-sm-2">Show/Hide: ${hide_show_elements.join(" | ")}</div>`
+
+    // Click event listener on the above links
+    document.querySelectorAll('a.toggle-vis').forEach(el => {
+        el.addEventListener('click', e => {
+            e.preventDefault();
+            const columnIdx = e.target.dataset.column;
+            const column = resp_table.column(columnIdx);
+            column.visible(!column.visible());
+        });
+    })
+
+}
+
 // Datatable init/config for responses table
 const resp_table = $("#individualResponsesTable").DataTable({
     order: [[4, 'desc']], // Sort on "Date" column
     columnDefs: [
         // add class to text search columns
-        { className: "column-text-search", targets: [1,2,3] }, 
+        { className: "column-text-search", targets: [1, 2, 3] },
         // For "Date" column, set type and don't wrap "Date" column. For "Time Elapsed" column, sort by "Date" column's data.
-        { className: "dt-nowrap", type: "date", orderData: 4, targets: [4,5] }, 
+        { className: "dt-nowrap", type: "date", orderData: 4, targets: [4, 5] },
     ],
+    autoWidth: false, // prevents hide/show cols from growing table
+    dom: "<'row'<'col-sm-12 col-md-4'l><'show-hide-cols col-sm-12 col-md-4'><'col-sm-12 col-md-4'f>><'row dt-row'<'col-sm-12'tr>><'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>",
     initComplete: function () {
         // Apply the text search to any column with the class "column-text-search"
         this.api()
@@ -62,9 +84,10 @@ const resp_table = $("#individualResponsesTable").DataTable({
                         column
                             .search(this.value)
                             .draw();
-                        }
-                    });
+                    }
+                });
             });
+        showHideColumns();
     },
 });
 
@@ -72,40 +95,40 @@ const resp_table = $("#individualResponsesTable").DataTable({
 setupDataTableDates("individualResponsesTable", 4, "dateRangeFilter");
 
 function updateInfoBox(index) {
-  // Select table rows of response details table.
-  const rows = document
-    .querySelector(`#response-summary-${index}`)
-    .querySelectorAll('table tbody tr');
+    // Select table rows of response details table.
+    const rows = document
+        .querySelector(`#response-summary-${index}`)
+        .querySelectorAll('table tbody tr');
 
-  // construct parent ID
-  const parentName = rows[13].children[1].textContent
-    .toLowerCase()
-    .split(" ")
-    .map(el => ([...el]
-      .filter(v => /[a-z\-]/.test(v)))
-      .join(""))
-    .join("-");
-  const parentId = `${rows[12].children[1].textContent}-${parentName || "anonymous"}`;
-  const parentUUID = rows[11].children[1].textContent
-  
-  // Recipient ID for sending message URL
-  const url = new URL(document.querySelector('.contact-family').href);
-  url.searchParams.set("recipient",parentUUID);
+    // construct parent ID
+    const parentName = rows[13].children[1].textContent
+        .toLowerCase()
+        .split(" ")
+        .map(el => ([...el]
+            .filter(v => /[a-z\-]/.test(v)))
+            .join(""))
+        .join("-");
+    const parentId = `${rows[12].children[1].textContent}-${parentName || "anonymous"}`;
+    const parentUUID = rows[11].children[1].textContent
 
-  // Short ID for child
-  document.querySelector('.short-child-id').textContent =
-    rows[15].children[1].textContent;
+    // Recipient ID for sending message URL
+    const url = new URL(document.querySelector('.contact-family').href);
+    url.searchParams.set("recipient", parentUUID);
 
-  // Create date for response with formatted date
-  document.querySelector('.response-date').textContent = moment(
-    rows[2].children[1].textContent
-  ).format('M/D/YYYY h:m A');
+    // Short ID for child
+    document.querySelector('.short-child-id').textContent =
+        rows[15].children[1].textContent;
 
-  // Parent Feedback
-  document.querySelector('.parent-feedback').textContent =
-    rows[6].children[1].textContent;
+    // Create date for response with formatted date
+    document.querySelector('.response-date').textContent = moment(
+        rows[2].children[1].textContent
+    ).format('M/D/YYYY h:m A');
 
-  // Send message to family URL
-  document.querySelector('.parent-id').textContent = parentId;
-  document.querySelector('.contact-family').href = url.toString();
+    // Parent Feedback
+    document.querySelector('.parent-feedback').textContent =
+        rows[6].children[1].textContent;
+
+    // Send message to family URL
+    document.querySelector('.parent-id').textContent = parentId;
+    document.querySelector('.contact-family').href = url.toString();
 }
