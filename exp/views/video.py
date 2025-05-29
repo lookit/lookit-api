eade2f610caa3a0f8f9c1295076c2572839e633c
Becky Gilbert <beckyannegilbert@gmail.com>
2025-05-29 15:32:14 -0700

    move payload processing into try/catch to handle key errors / missing attributes

--- exp/views/video.py
+++ exp/views/video.py
@@ -5,7 +5,12 @@ import json
 
 from botocore.exceptions import ClientError
 from django.conf import settings
-from django.http import HttpResponse, HttpResponseForbidden, HttpResponseNotFound
+from django.http import (
+    HttpResponse,
+    HttpResponseBadRequest,
+    HttpResponseForbidden,
+    HttpResponseNotFound,
+)
 from django.utils.decorators import method_decorator
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
@@ -28,43 +33,52 @@ class RenameVideoView(View):
         # of events in the future; some of the Pipe event data includes semicolons which
         # throw the automatic parsing for a loop.
 
-        # Authenticate the webhook (see https://addpipe.com/docs#authenticating-webhooks)
         key = settings.PIPE_WEBHOOK_KEY
-        # Append the JSON POST data received via webhook to the URL string.
-        thisURL = request.scheme + "://" + request.headers["host"] + request.path
-        payload = request.POST.get("payload")
-        message = thisURL + payload
-        # Hash the resulting string with HMAC-SHA1, using the webhook authentication key; generate binary signature.
-        keyBytes = bytes(key, "UTF-8")
-        messageBytes = bytes(message, "UTF-8")
-        digester = hmac.new(keyBytes, messageBytes, hashlib.sha1)
-        signatureBinary = digester.digest()
-        # Base64 encode the binary signature.
-        signatureComputed = base64.b64encode(signatureBinary)
-        # Compare to the one in the header
-        signatureSent = bytes(request.headers["x-pipe-signature"], "UTF-8")
-        authenticated = signatureComputed == signatureSent
 
-        # Convert data from string representation of dict to dict
-        payload_data = json.loads(payload)
+        try:
+            # Authenticate the webhook (see https://addpipe.com/docs#authenticating-webhooks)
+            # Append the JSON POST data received via webhook to the URL string.
+            thisURL = request.scheme + "://" + request.headers["host"] + request.path
+            payload = request.POST.get("payload")
+            message = thisURL + payload
+            # Hash the resulting string with HMAC-SHA1, using the webhook authentication key; generate binary signature.
+            keyBytes = bytes(key, "UTF-8")
+            messageBytes = bytes(message, "UTF-8")
+            digester = hmac.new(keyBytes, messageBytes, hashlib.sha1)
+            signatureBinary = digester.digest()
+            # Base64 encode the binary signature.
+            signatureComputed = base64.b64encode(signatureBinary)
+            # Compare to the one in the header
+            signatureSent = bytes(request.headers["x-pipe-signature"], "UTF-8")
+            authenticated = signatureComputed == signatureSent
 
-        if authenticated and (
-            payload_data["data"]["s3UploadStatus"] == "upload success"
-        ):  # Go ahead and move the file
-            new_name = payload_data["data"]["payload"]
+            # Convert data from string representation of dict to dict
+            payload_data = json.loads(payload)
 
-            if not new_name:  # Make sure we don't have an empty payload string
+            if authenticated and (
+                payload_data["data"]["s3UploadStatus"] == "upload success"
+            ):  # Go ahead and move the file
+                new_name = payload_data["data"]["payload"]
+
+                if not new_name:  # Make sure we don't have an empty payload string
+                    return HttpResponseForbidden()
+
+                try:
+                    video_obj = Video.from_pipe_payload(payload_data)
+                    return HttpResponse(
+                        (
+                            payload_data["data"]["videoName"]
+                            + " --> "
+                            + video_obj.filename
+                        )
+                        if video_obj
+                        else "Preview video not saved"
+                    )
+                except ClientError as e:
+                    return HttpResponseNotFound(e)
+
+            else:  # Not authenticated
                 return HttpResponseForbidden()
 
-            try:
-                video_obj = Video.from_pipe_payload(payload_data)
-                return HttpResponse(
-                    (payload_data["data"]["videoName"] + " --> " + video_obj.filename)
-                    if video_obj
-                    else "Preview video not saved"
-                )
-            except ClientError as e:
-                return HttpResponseNotFound(e)
-
-        else:  # Not authenticated
-            return HttpResponseForbidden()
+        except KeyError as e:
+            return HttpResponseBadRequest(f"Missing expected header or field: {str(e)}")
