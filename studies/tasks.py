d35395c9cedefda1710ea7bbdf54d6cf36a12207
Becky Gilbert <beckyannegilbert@gmail.com>
2025-08-19 14:59:46 -0700

    remove old logging

--- studies/tasks.py
+++ studies/tasks.py
@@ -329,16 +329,8 @@ def cleanup_docker_containers():
 def build_zipfile_of_videos(
     self, filename, study_uuid, match, requesting_user_uuid, consent_only=False
 ):
-    import getpass
-    import os
-    import socket
-
     from accounts.models import User
     from studies.models import Study
-
-    logging.info(f"Hostname {socket.gethostname()}")
-    logging.info(f"User {getpass.getuser()}")
-    logging.info(f"Current directory {os.getcwd()}")
 
     study = Study.objects.get(uuid=study_uuid)
     requesting_user = User.objects.get(uuid=requesting_user_uuid)
