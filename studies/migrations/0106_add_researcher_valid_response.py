f08c9239b859448aba08d74a410e6f020ed19366
Becky Gilbert <beckyannegilbert@gmail.com>
2026-05-26 11:01:59 -0700

    switch to snake case for local vars - fixes sonarqube issue

--- studies/migrations/0106_add_researcher_valid_response.py
+++ studies/migrations/0106_add_researcher_valid_response.py
@@ -18,8 +18,8 @@ def compute_is_tallied(apps, schema_editor):
 
     All responses start as False (the field default). This function marks passing responses True.
     """
-    Response = apps.get_model("studies", "Response")
-    ConsentRuling = apps.get_model("studies", "ConsentRuling")
+    response_model = apps.get_model("studies", "Response")
+    consent_ruling_model = apps.get_model("studies", "ConsentRuling")
 
     # Filter on eligibility (must be "Eligible" or an empty array for backwards compatibility)
     eligible_filter = models.Q(eligibility=[]) | models.Q(
@@ -27,19 +27,19 @@ def compute_is_tallied(apps, schema_editor):
     )
 
     # Change is_tallied for external study responses to True if: non-preview, eligible
-    Response.objects.filter(
+    response_model.objects.filter(
         study__study_type_id=EXTERNAL_STUDY_TYPE_ID,
         is_preview=False,
     ).filter(eligible_filter).update(is_tallied=True)
 
     # Change is_tallied internal study responses to True if: non-preview, eligible, completed, has consent, consent not rejected
     newest_ruling_subquery = models.Subquery(
-        ConsentRuling.objects.filter(response=models.OuterRef("pk"))
+        consent_ruling_model.objects.filter(response=models.OuterRef("pk"))
         .order_by("-created_at")
         .values("action")[:1]
     )
     tallied_internal_ids = list(
-        Response.objects.exclude(study__study_type_id=EXTERNAL_STUDY_TYPE_ID)
+        response_model.objects.exclude(study__study_type_id=EXTERNAL_STUDY_TYPE_ID)
         .filter(is_preview=False, completed=True, completed_consent_frame=True)
         .filter(eligible_filter)
         .annotate(current_ruling=newest_ruling_subquery)
@@ -47,7 +47,9 @@ def compute_is_tallied(apps, schema_editor):
         .values_list("id", flat=True)
     )
     if tallied_internal_ids:
-        Response.objects.filter(id__in=tallied_internal_ids).update(is_tallied=True)
+        response_model.objects.filter(id__in=tallied_internal_ids).update(
+            is_tallied=True
+        )
 
 
 class Migration(migrations.Migration):
