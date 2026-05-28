fc46287e56de618d9cb9e6b2ebcb592df0084824
Becky Gilbert <beckyannegilbert@gmail.com>
2026-05-28 13:29:08 -0700

    updates to tally status change modal text

--- web/static/js/study-responses.js
+++ web/static/js/study-responses.js
@@ -53,9 +53,9 @@ if (hasMaxResponses) document.getElementById('tallyConfirmModal').addEventListen
         additional_text = '<div></div>';
     } else {
         // additional text if changing tallied to untallied
-        additional_text = '<div class="text-muted mt-3">If the correct child participated, was eligible, completed the study, and the family participated in good faith (not a confirmed spammer), then the response should be tallied. This is true even if you are not able to use this response in your analysis, for instance because the child was fussy or there were technical problems.';
+        additional_text = '<div class="text-muted mt-3">Responses should be tallied if:<ul><li>The correct child participated</li><li>The child was eligible for the study</li><li>The family participated in good faith (not a confirmed scammer)</li></ul>This is true even if you are not able to use this response in your analysis, for instance because the child was fussy or there were technical problems.';
         if (isExternal) {
-            additional_text += '<br><br>The response should NOT be tallied if, in this response, the family:<ul><li>did not complete the study during this session (asynchronous studies)</li><li>did not schedule a time to participate, or scheduled a session but did not show up (synchronous studies)</li></ul>';
+            additional_text += '<br><br>For external studies, you may un-tally a response if, in this response session, the family:<ul><li>Did not complete the study (asynchronous studies)</li><li>Did not schedule a time to participate (synchronous studies)</li></ul>';
         }
         additional_text += '</div>';
     }
